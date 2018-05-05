5bbe6219d5ed4f1850871825af98b9ffbe4c9359
Idolin <dk.1997-fast@yandex.ru>
2018-05-05 14:53:25 +0300

    none moved to t_useful added value in enum valueType for _rank<unsigned> to use for templates (template<_rank<x>::valueType ignore>)

--- src/template/t_useful.hpp
+++ src/template/t_useful.hpp
@@ -1,10 +1,28 @@
 #pragma once
 
+struct none
+{
+    none()
+    {}
+
+    none(int)
+    {}
+};
+
 template<unsigned N>
 struct _rank: _rank<N - 1>
-{};
+{
+    enum valueType
+    {
+        value
+    };
+};
 
 template<>
 struct _rank<0>
-{};
-
+{
+    enum valueType
+    {
+        value
+    };
+};
