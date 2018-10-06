e0bf2b0e2ff9da8481458db5397a0c4faac06995
Idolin <dk.1997-fast@yandex.ru>
2018-10-06 19:18:23 +0300

    mergesort was optimized + it can now count inversions
    quicksort was a bit optimized + function to use to get element for partition now can be set in template(random one from array by default)

--- src/sort/sortlog.hpp
+++ src/sort/sortlog.hpp
@@ -41,72 +41,66 @@ void merge_in_place(T *start, unsigned part_len1, unsigned part_len2)
     // ???
 }
 
-template<typename T, typename compare_func<T>::type compare = _less<T>>
-void mergesort(T *start, T *end)
-{
-    unsigned dif = end - start;
-    T *swap_array = new T[dif];
-    for(unsigned i = 1; i < dif; i += 2)
-        if(compare(start[i], start[i - 1]))
-            std::swap(start[i - 1], start[i]);
-    unsigned block_size = 2;
-    while(block_size < dif)
-    {
-        unsigned i = 0, next = block_size * 2;
-        while(next <= dif)
-        {
-            _copy(swap_array, block_size, start + i);
-            merge_two_arrays<T, compare, true>(swap_array, block_size, start + i + block_size, block_size, start + i);
-            i = next;
-            next += block_size * 2;
-        }
-        if(i + block_size < dif)
-        {
-            _copy(swap_array, block_size, start + i);
-            merge_two_arrays<T, compare, true>(swap_array, block_size, start + i + block_size, dif - i - block_size,
-                                               start + i);
-        }
-        block_size *= 2;
-    }
-    delete[] swap_array;
-}
-
-template<typename T, typename compare_func<T>::type compare = _less<T>>
-void mergesort_v2(T *start, T *end)
+template<typename T, typename compare_func<T>::type compare = _less<T>, bool count_inversions = false>
+typename std::conditional<count_inversions, unsigned long long, void>::type
+mergesort(T *start, T *end)
 {
     unsigned dif = end - start;
     if(dif < 2)
-        return;
+        return static_cast<typename std::conditional<count_inversions, unsigned long long, void>::type>(0);
+    unsigned long long inversions = 0;
     unsigned max_block_size = to2(dif) >> 1, block_size = 2, i = 1;
     T *swap_array = new T[max_block_size];
     for(; i < dif; i += 2)
         if(compare(start[i], start[i - 1]))
+        {
             std::swap(start[i - 1], start[i]);
-    if((i-- == dif) && compare(start[i], start[i - 1]))
-    {
-        if(compare(start[i], start[i - 2]))
-            std::swap(start[i - 2], start[i]);
-        std::swap(start[i - 1], start[i]);
-    }
+            if(count_inversions)
+                inversions++;
+        }
     while(block_size <= max_block_size)
     {
         for(i = block_size;i < dif - block_size;i += block_size * 2)
             if(compare(start[i], start[i - 1]))
             {
                 _copy(swap_array, block_size, start + i - block_size);
-                merge_two_arrays<T, compare, true>(swap_array, block_size, start + i, block_size, start + i - block_size);
+                if(count_inversions)
+                    inversions += merge_two_arrays<T, compare, true, true>(swap_array,
+                                                        block_size, start + i, block_size, start + i - block_size);
+                else
+                    merge_two_arrays<T, compare>(swap_array, block_size, start + i, block_size,
+                                                 start + i - block_size);
             }
         if(i < dif)
         {
             _copy(swap_array, block_size, start + i - block_size);
-            merge_two_arrays<T, compare, true>(swap_array, block_size, start + i, dif - i, start + i - block_size);
+            if(count_inversions)
+                inversions += merge_two_arrays<T, compare, true, true>(swap_array,
+                                                        block_size, start + i, dif - i, start + i - block_size);
+            else
+                merge_two_arrays<T, compare, true>(swap_array, block_size, start + i,
+                                                   dif - i, start + i - block_size);
         }
         block_size *= 2;
     }
     delete[] swap_array;
+    if(count_inversions)
+        return static_cast<typename std::conditional<count_inversions, unsigned long long, void>::type>(inversions);
+    else
+        return static_cast<typename std::conditional<count_inversions, unsigned long long, void>::type>(0);
 }
 
-template<typename T, typename compare_func<T>::type compare = _less<T>>
+namespace
+{
+    template<typename T>
+    typename def_get_by<T>::type _get_random(const T *start, const T *end)
+    {
+        return *(start + randomA(end - start));
+    }
+}
+
+template<typename T, typename compare_func<T>::type compare = _less<T>,
+    typename def_get_by<T>::type (*get_delim)(const T*, const T*) = _get_random<T>>
 void quicksort(T *start, T *end)
 {
     if(end - start < 17)
@@ -114,10 +108,14 @@ void quicksort(T *start, T *end)
         insertionsort<T, compare>(start, end);
         return;
     }
-    T df = *(start + (end - start) / 2);
+    typename def_get_by<T>::type df = get_delim(start, end);
     T *sp = partition<T, compare>(start, end, df);
-    quicksort<T, compare>(start, sp);
-    quicksort<T, compare>(sp, end);
+    T *sb = sp;
+    while(sb > start && !compare(*--sb, df));
+    quicksort<T, compare, get_delim>(start, sb + 1);
+    while(sp < end && !compare(df, *sp))
+        sp++;
+    quicksort<T, compare, get_delim>(sp, end);
 }
 
 //void timsort()
