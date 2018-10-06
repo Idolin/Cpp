8494dc487d7c44ca1baa04461a9610eee9aec8bd
Idolin <dk.1997-fast@yandex.ru>
2018-10-06 19:25:44 +0300

    Added ConditionalStored struct

--- src/struct/optional.hpp
+++ src/struct/optional.hpp
@@ -2,6 +2,7 @@
 
 #include "../debug/def_debug.h"
 #include "../template/t_useful.hpp"
+#include "../template/typemethods.hpp"
 
 template<typename T>
 struct optional
@@ -55,4 +56,67 @@ struct optional<none>
     {
         return is_set;
     }
+};
+
+template<typename T, bool store>
+struct ConditionalStored;
+
+template<typename T>
+struct ConditionalStored<T, true>
+{
+private:
+    T value;
+
+public:
+    ConditionalStored()
+    {}
+
+    ConditionalStored(typename def_get_by<T>::type x): value(x)
+    {}
+
+    typename def_get_by<T>::type operator=(const ConditionalStored<T, true> &otr)
+    {
+        value = otr.value;
+    }
+
+    typename def_get_by<T>::type operator=(ConditionalStored<T, true> &&otr)
+    {
+        value = otr.value;
+    }
+
+    typename def_get_by<T>::type get_value() const
+    {
+        return value;
+    }
+
+    void set_value(typename def_get_by<T>::type x)
+    {
+        value = x;
+    }
+};
+
+template<typename T>
+struct ConditionalStored<T, false>
+{
+public:
+    ConditionalStored()
+    {}
+
+    ConditionalStored(typename def_get_by<T>::type x)
+    {}
+
+    typename def_get_by<T>::type operator=(const ConditionalStored<T, true> &otr)
+    {}
+
+    typename def_get_by<T>::type operator=(ConditionalStored<T, true> &&otr)
+    {}
+
+    typename def_get_by<T>::type get_value() const //useful only for templates, not expected to be called in runtime
+    {
+        ASSERT(false);
+        return (T)(0);
+    }
+
+    void set_value(typename def_get_by<T>::type x)
+    {}
 };
