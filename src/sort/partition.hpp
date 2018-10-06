6f435255e26afc3704c14651855a8f6f4ddacb14
Idolin <dk.1997-fast@yandex.ru>
2018-10-06 19:14:16 +0300

    partition_3 function was removed
    In partition: *end is using instead of *ep

--- src/sort/partition.hpp
+++ src/sort/partition.hpp
@@ -2,61 +2,19 @@
 
 #include "../template/commonmethods.hpp"
 #include "../template/typemethods.hpp"
+#include <algorithm>
 #include <functional>
-
-template<typename T, typename compare_func<T>::type compare = _less<T>>
-T *partition_3(T *start, const T *end, T &df, unsigned &count, T &min)
-{
-    T *sp = start;
-    if(count > 0)
-    {
-        if(not(compare(df, min) or compare(min, df)))
-            df++;
-        sp += count;
-        count = 0;
-        T *ep = sp;
-        while(ep < end)
-            if(not compare(df, *ep))
-            {
-                if(not(compare(*ep, min) or compare(min, *ep)))
-                {
-                    while(not(compare(*start, min) or compare(min, *start)))
-                        start++;
-                    _swap(start++, ep);
-                } else
-                {
-                    if(compare(*ep, df))
-                        _swap(sp++, ep);
-                    else
-                        count++;
-                    ep++;
-                }
-            } else
-                ep++;
-    } else
-    {
-        for(T *ep = sp; ep < end; ep++)
-            if(not compare(df, *ep))
-            {
-                if(compare(*ep, df))
-                    _swap(sp++, ep);
-                else
-                    count++;
-            }
-    }
-    return sp;
-}
 
 template<typename T, typename compare_func<T>::type compare = _less<T>>
 T *partition(T *start, T *end, T df)
 {
-    for(T *ep = end; start < ep; start++)
-        if(not compare(*start, df))
+    for(;start < end;start++)
+        if(!compare(*start, df))
         {
-            while(compare(df, *--ep))
-                if(start == ep)
+            while(compare(df, *--end))
+                if(start == end)
                     return start;
-            std::swap(*start, *ep);
+            std::swap(*start, *end);
         }
-    return start;
+    return end;
 }
