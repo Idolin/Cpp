354dec62ab9a8ed75fdf6b31f33e1520d6222208
Idolin <dk.1997-fast@yandex.ru>
2018-11-11 19:25:31 +0300

    Vector tests added

---
+++ src/container/vector.test.cpp
@@ -0,0 +1,22 @@
+#include "../debug/test_def.h"
+#include "../other/rand.h"
+#include "vector.hpp"
+
+TEST_PACK(vector)
+{
+    TEST(add_get_elements)
+    {
+        vect<int> v;
+        v.push(0);
+        for(int i = 1;i < 10000;i++)
+        {
+            if(randomUC() < 20)
+            {
+                int index = randomA<int>(i);
+                EXPECT_EQ(v.at(index), index);
+            }
+            v.push(i);
+        }
+        EXPECT_EQ(v.at(9999), 9999);
+    }
+}
