b845629bd32fd42f20000a4b60e5cffca38c3cb0
Idolin <dk.1997-fast@yandex.ru>
2019-04-04 16:01:49 +0300

    Binary search rewritten using binary_search struct

--- src/struct_algo/binary_search.hpp
+++ src/struct_algo/binary_search.hpp
@@ -1,7 +1,29 @@
 #pragma once
 
+#include <algorithm>
+#include <stdexcept>
+#include <type_traits>
+
+#include "../debug/def_debug.h"
+#include "../template/struct_tags.hpp"
+#include "../template/typemethods.hpp"
+
 namespace
 {
+    template<typename T, typename Enable = typename std::enable_if_t<is_dereferencable<T>::value>>
+    inline auto _get(T p) -> typename def_get_by<typename std::remove_reference<decltype(*p)>::type>::type
+    {
+        return *p;
+    }
+
+    template<typename T, typename Enable = typename std::enable_if_t<!is_dereferencable<T>::value>>
+    inline bool _get(T p)
+    {
+        ASSERT(false, "No default method to get value from type %s", typeid(T).name());
+        throw std::runtime_error("binary_search: No default method to get value from given type");
+        return false;
+    }
+
     template<typename T, typename Enable = void>
     struct __binary_search_base;
 
@@ -9,6 +31,10 @@ namespace
     struct __binary_search_base<T, typename std::enable_if<std::is_pointer<T>::value>::type>
     {
         T left_border, right_border, cur;
+
+        __binary_search_base(T left, T right, T current): left_border(left),
+                                                right_border(right), cur(current)
+        {}
 
         __binary_search_base(T left, T right): left_border(left), right_border(right),
                                                cur(middle())
@@ -19,19 +45,15 @@ namespace
             return left_border + (right_border - left_border) / 2;
         }
 
-        int epsilon() const
+        static int epsilon()
         {
             return 1;
         }
 
         void end_search()
         {
-            right_border = nullptr;
-        }
-
-        bool is_done() const
-        {
-            return right_border == nullptr;
+            left_border = reinterpret_cast<T>(1);
+            right_border = reinterpret_cast<T>(0);
         }
     };
 
@@ -40,6 +62,10 @@ namespace
                                                            std::is_unsigned<T>::value>::type>
     {
         T left_border, right_border, cur;
+
+        __binary_search_base(T left, T right, T current): left_border(left),
+                                                right_border(right), cur(current)
+        {}
 
         __binary_search_base(T left, T right): left_border(left), right_border(right),
                                                 cur(middle())
@@ -50,20 +76,15 @@ namespace
             return left_border + (right_border - left_border) / 2;
         }
 
-        T epsilon() const
+        static T epsilon()
         {
             return 1;
-        };
+        }
 
         void end_search()
         {
             left_border = 1;
-            right_border = cur = 0;
-        }
-
-        bool is_done() const
-        {
-            return left_border > right_border;
+            right_border = 0;
         }
     };
 
@@ -72,6 +93,10 @@ namespace
                                                            std::is_signed<T>::value>::type>
     {
         T left_border, right_border, cur;
+
+        __binary_search_base(T left, T right, T current): left_border(left),
+                                                right_border(right), cur(current)
+        {}
 
         __binary_search_base(T left, T right): left_border (left), right_border(right),
                                                cur(middle())
@@ -83,7 +108,7 @@ namespace
             return left_border / 2 + right_border / 2 + (left_border & right_border & 0b1);
         }
 
-        T epsilon() const
+        static T epsilon()
         {
             return 1;
         }
@@ -91,12 +116,7 @@ namespace
         void end_search()
         {
             left_border = 1;
-            right_border = cur = 0;
-        }
-
-        bool is_done() const
-        {
-            return left_border > right_border;
+            right_border = 0;
         }
     };
 
@@ -105,6 +125,10 @@ namespace
             std::is_floating_point<T>::value>::type>
     {
         T left_border, right_border, cur;
+
+        __binary_search_base(T left, T right, T current): left_border(left),
+                                                right_border(right), cur(current)
+        {}
 
         __binary_search_base(T left, T right): left_border (left), right_border(right),
                                                cur(middle())
@@ -116,7 +140,7 @@ namespace
             return left_border / 2 + right_border / 2;
         }
 
-        T epsilon() const
+        static T epsilon()
         {
             return std::numeric_limits<T>::epsilon();
         }
@@ -124,12 +148,7 @@ namespace
         void end_search()
         {
             left_border = 1;
-            right_border = cur = 0;
-        }
-
-        bool is_done() const
-        {
-            return left_border > right_border;
+            right_border = 0;
         }
     };
 }
@@ -137,38 +156,221 @@ namespace
 template<typename T>
 struct binary_search: protected __binary_search_base<T>
 {
-    binary_search(T left, T right): __binary_search_base<T>(left, right)
+    //include_last - if true than search in range [left, right], false - [left, right)
+    binary_search(T left, T right, bool include_last=false):
+        __binary_search_base<T>(left, include_last ? right : right - __binary_search_base<T>::epsilon())
+    {
+        if(!include_last && left >= right)
+            this->end_search();
+    }
+
+    binary_search(const binary_search<T> &otr):
+        __binary_search_base<T>(otr.left_border, otr.right_border, otr.cur)
     {}
 
-    T current() const
+    binary_search<T>& operator=(const binary_search<T> &otr)
+    {
+        this->left_border = otr.left_border; // not much sense in checking for equality as
+        this->cur = otr.cur; // all fields are fast to assign
+        this->right_border = otr.right_border;
+        return *this;
+    }
+
+    bool operator==(const binary_search<T> &otr) const
+    {
+        return (this->cur == otr.cur) && (this->left_border == otr.left_border) &&
+            (this->right_border == otr.right_border);
+    }
+
+    bool operator!=(const binary_search<T> &otr) const
+    {
+        return !(*this == otr);
+    }
+
+    T current()
     {
         return this->cur;
     }
 
+    const T current() const
+    {
+        return this->cur;
+    }
+
+    //go to left half
     void left()
     {
         if(this->left_border < this->cur)
-        {
-            this->right_border = this->cur - this->epsilon();
-            this->cur = this->middle();
-        }
+            _left();
         else
             this->end_search();
     }
 
+    //go to right half
     void right()
     {
         if(this->cur < this->right_border)
-        {
-            this->left_border = this->cur + this->epsilon();
-            this->cur = this->middle();
-        }
+            _right();
         else
             this->end_search();
     }
 
+    //binary search finished
     bool done() const
     {
-        return this->is_done();
+        return this->left_border > this->right_border;
+    }
+
+    //is not done
+    operator bool() const
+    {
+        return !done();
+    }
+
+    //if get_last_not -> get last element where condition not true(or previous to first)
+    //first iterator: *it >= val or next to last, done() returns false if position found and true otherwise
+    template<typename R = decltype(_get<T>(std::declval<T>())),
+        typename compare_func<R>::type compare = _less<R>, R(*get)(T) = _get<T>>
+    T upper_bound(typename def_get_by<R>::type val, bool get_last_not = false)
+    {
+        if(done())
+            return this->cur;
+        const T last = (get_last_not) ? this->left_border : this->right_border;
+        while(true)
+            if(compare(val, get(this->cur)))
+            {
+                if(this->left_border < this->cur)
+                    _left();
+                else
+                {
+                    if(get_last_not)
+                    {
+                        this->cur -= this->epsilon();
+                        if(last == this->left_border)
+                            this->end_search();
+                    }
+                    return this->cur;
+                }
+            }
+            else
+            {
+                if(this->cur < this->right_border)
+                    _right();
+                else
+                {
+                    if(!get_last_not)
+                    {
+                        this->cur += this->epsilon();
+                        if(last == this->right_border)
+                            this->end_search();
+                    }
+                    return this->cur;
+                }
+            }
+    }
+
+    //lower_bound, but also returns not done() with value as std::pair
+    template<typename R = typename std::remove_pointer<T>::type,
+        typename compare_func<R>::type compare = _less<R>, R(*get)(T) = _get<T>>
+    std::pair<T, bool> upper_bound_check(typename def_get_by<R>::type val, bool get_last_not = false)
+    {
+        upper_bound<R, compare, get>(val, get_last_not);
+        return std::make_pair(this->cur, !done());
+    }
+
+    template<typename R = typename std::remove_pointer<T>::type,
+        typename compare_func<R>::type compare = _less<R>, R(*get)(T) = _get<T>>
+    size_t upper_bound_pos(typename def_get_by<R>::type val, bool get_last_not = false)
+    {
+        const T left = this->left_border;
+        return upper_bound<R, compare, get>(val, get_last_not) - left;
+    }
+
+    template<typename R = typename std::remove_pointer<T>::type,
+        typename compare_func<R>::type compare = _less<R>, R(*get)(T) = _get<T>>
+    std::pair<size_t, bool> upper_bound_pos_check(typename def_get_by<R>::type val, bool get_last_not = false)
+    {
+        size_t pos = upper_bound_pos<R, compare, get>(val, get_last_not);
+        return std::make_pair(pos, !done());
+    }
+
+    //first iterator: *it > val or next to last, done() returns false if position found and true otherwise
+    template<typename R = typename std::remove_pointer<T>::type,
+        typename compare_func<R>::type compare = _less<R>, R(*get)(T) = _get<T>>
+    T lower_bound(typename def_get_by<R>::type val, bool get_last_not = false)
+    {
+        return upper_bound<R, _not_comp<R, _swap_comp<R, compare>>, get>(val, get_last_not);
+    }
+
+    //upper_bound, but also returns not done() with value as std::pair
+    template<typename R = typename std::remove_pointer<T>::type,
+        typename compare_func<R>::type compare = _less<R>, R(*get)(T) = _get<T>>
+    std::pair<T, bool> lower_bound_check(typename def_get_by<R>::type val, bool get_last_not = false)
+    {
+        lower_bound<R, compare, get>(val, get_last_not);
+        return std::make_pair(this->cur, !done());
+    }
+
+    template<typename R = typename std::remove_pointer<T>::type,
+        typename compare_func<R>::type compare = _less<R>, R(*get)(T) = _get<T>>
+    size_t lower_bound_pos(typename def_get_by<R>::type val, bool get_last_not = false)
+    {
+        const T left = this->left_border;
+        return lower_bound<R, compare, get>(val, get_last_not) - left;
+    }
+
+    template<typename R = typename std::remove_pointer<T>::type,
+        typename compare_func<R>::type compare = _less<R>, R(*get)(T) = _get<T>>
+    std::pair<size_t, bool> lower_bound_pos_check(typename def_get_by<R>::type val, bool get_last_not = false)
+    {
+        size_t pos = lower_bound_pos<R, compare, get>(val, get_last_not);
+        return std::make_pair(pos, !done());
+    }
+
+    //returns first and last iterator values of which equal to val or [l, r] such thar l > r if not found,
+    //if found done() = false, otherwise done() is true
+    template<typename R = typename std::remove_pointer<T>::type,
+        typename compare_func<R>::type compare = _less<R>, R(*get)(T) = _get<T>>
+    std::pair<T, T> bounds(typename def_get_by<R>::type val)
+    {
+        if(done())
+            return std::make_pair(this->left_border, this->right_border);
+        const T first = this->left_border;
+        T right = upper_bound<R, compare, get>(val, true);
+        if(done())
+            return std::make_pair(this->left_border, this->right_border);
+        this->left_border = first;
+        T left = lower_bound<R, compare, get>(val);
+        if(done() || left > right)
+        {
+            this->end_search();
+            return std::make_pair(this->left_border, this->right_border);
+        }
+        return std::make_pair(left, right);
+    }
+
+    //bounds pos or [l, r] such that l > r if no val found
+    template<typename R = typename std::remove_pointer<T>::type,
+        typename compare_func<R>::type compare = _less<R>, R(*get)(T) = _get<T>>
+    std::pair<size_t, size_t> bounds_pos(typename def_get_by<R>::type val)
+    {
+        const T left = this->left_border;
+        std::pair<T, T> range = bounds<R, compare, get>(val);
+        if(!done())
+            return std::make_pair(range.first - left, range.second - left);
+        return std::make_pair(1, 0);
+    }
+
+private:
+    void _left()
+    {
+        this->right_border = this->cur - this->epsilon();
+        this->cur = this->middle();
+    }
+
+    void _right()
+    {
+        this->left_border = this->cur + this->epsilon();
+        this->cur = this->middle();
     }
 };
--- src/struct_algo/binary_search.test.cpp
+++ src/struct_algo/binary_search.test.cpp
@@ -5,7 +5,7 @@ TEST_PACK(binary_search)
 {
     TEST(int_type)
     {
-        binary_search<int> b(0, 8);
+        binary_search<int> b(0, 8, true);
         EXPECT_FALSE(b.done());
         EXPECT_EQ(b.current(), 4);
         b.left();
@@ -27,7 +27,7 @@ TEST_PACK(binary_search)
 
     TEST(empty_range)
     {
-        binary_search<int> b(0, 0);
+        binary_search<int> b(0, 0, true);
         EXPECT_FALSE(b.done());
         EXPECT_EQ(b.current(), 0);
         b.right();
@@ -36,7 +36,7 @@ TEST_PACK(binary_search)
 
     TEST(bad_range)
     {
-        binary_search<int> b(10, 9);
+        binary_search<int> b(10, 9, true);
         EXPECT_TRUE(b.done());
         b.left();
         EXPECT_TRUE(b.done());
@@ -44,7 +44,7 @@ TEST_PACK(binary_search)
 
     TEST(full_range)
     {
-        binary_search<int> b(std::numeric_limits<int>::lowest(), std::numeric_limits<int>::max());
+        binary_search<int> b(std::numeric_limits<int>::lowest(), std::numeric_limits<int>::max(), true);
         EXPECT_FALSE(b.done());
         unsigned n = 0;
         unsigned m = sizeof(int) * 8;
@@ -58,7 +58,7 @@ TEST_PACK(binary_search)
 
     TEST(range_near_right_border)
     {
-        binary_search<int> b(std::numeric_limits<int>::max() - 72, std::numeric_limits<int>::max() - 46);
+        binary_search<int> b(std::numeric_limits<int>::max() - 72, std::numeric_limits<int>::max() - 46, true);
         EXPECT_FALSE(b.done());
         unsigned n = 0;
         unsigned m = 5;
@@ -72,7 +72,7 @@ TEST_PACK(binary_search)
 
     TEST(bool_range)
     {
-        binary_search<bool> b(false, true);
+        binary_search<bool> b(false, true, true);
         EXPECT_FALSE(b.done());
         if(b.current())
             b.left();
@@ -82,7 +82,7 @@ TEST_PACK(binary_search)
         b.right();
         EXPECT_TRUE(b.done());
 
-        binary_search<bool> b2(true, false);
+        binary_search<bool> b2(true, false, true);
         EXPECT_TRUE(b.done());
     }
 
@@ -91,7 +91,7 @@ TEST_PACK(binary_search)
         int *a = new int[137];
         for(int i = 0;i < 137;i++)
             a[i] = i;
-        binary_search<int*> b(a, a + 136);
+        binary_search<int*> b(a, a + 136, true);
         EXPECT_FALSE(b.done());
         EXPECT_EQ(*b.current(), 68);
         unsigned n = 0;
@@ -106,7 +106,7 @@ TEST_PACK(binary_search)
 
     TEST(float_range)
     {
-        binary_search<float> b(-3, 3);
+        binary_search<float> b(-3, 3, true);
         EXPECT_FALSE(b.done());
         EXPECT_FLOAT_EQ(b.current(), 0);
         unsigned n = 0;
@@ -121,7 +121,7 @@ TEST_PACK(binary_search)
 
     TEST(full_double_range)
     {
-        binary_search<double> b(std::numeric_limits<double>::lowest(), std::numeric_limits<double>::max());
+        binary_search<double> b(std::numeric_limits<double>::lowest(), std::numeric_limits<double>::max(), true);
         EXPECT_FALSE(b.done());
         EXPECT_DOUBLE_EQ(b.current(), 0, "Expected ~0 but got %.5lf\n", b.current());
         unsigned n = 0;
@@ -132,5 +132,277 @@ TEST_PACK(binary_search)
             n++;
         }
         EXPECT_IN_RANGE(n, m / 4 - 1, m, "Expected amount of steps(%u) to be in range: %u - %u", n, m / 2 - 1, m);
+    }
+
+    TEST(equals)
+    {
+        binary_search<int> a(1, 3, true);
+        binary_search<int> b(1, 3, true);
+        binary_search<int> c(1, 7, true);
+        EXPECT_EQ(a, b);
+        EXPECT_NE(b, c);
+        a.left();
+        EXPECT_NE(a, b);
+        b.left();
+        EXPECT_EQ(a, b);
+        c.left();
+        EXPECT_NE(b, c);
+        c.right();
+        c.right();
+        EXPECT_TRUE(c.done());
+        EXPECT_NE(a, c);
+        a.left();
+        EXPECT_TRUE(a.done());
+        EXPECT_TRUE(b, "(b) == !b.done()");
+        EXPECT_NE(a, c);
+        EXPECT_NE(a, b);
+        b.left();
+        EXPECT_EQ(a, b);
+        EXPECT_NE(b, c);
+    }
+
+    TEST(assign)
+    {
+        binary_search<int> a(1, 3);
+        binary_search<int> b = a;
+        binary_search<int> c(1, 5);
+        EXPECT_EQ(a, b);
+        EXPECT_NE(a, c);
+        a.left();
+        EXPECT_NE(a, b);
+        c = b;
+        EXPECT_NE(a, c);
+        EXPECT_EQ(b, c);
+        c.left();
+        EXPECT_EQ(a, c);
+        EXPECT_NE(a, b);
+        EXPECT_NE(b, c);
+    }
+
+    TEST(lower_bound)
+    {
+        int a[] = {0, 0, 1, 2, 2, 4};
+        binary_search<int*> b(&a[0], &a[sizeof(a) / sizeof(int)]);
+        auto t = b;
+        EXPECT_EQ(b.lower_bound(0) - (int*)&a, 0, "Expect 0, not %u", (b = t).lower_bound(0) - (int*)&a);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.lower_bound(3) - (int*)&a, 5);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.lower_bound(-9) - (int*)&a, 0);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.lower_bound(4) - (int*)&a, 5);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.lower_bound(5) - (int*)&a, 6);
+        EXPECT_FALSE(b);
+        b = t;
+
+        EXPECT_EQ(b.lower_bound(5, true) - (int*)&a, 5);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.lower_bound(0, true) - (int*)&a, -1);
+        EXPECT_FALSE(b);
+    }
+
+    TEST(upper_bound)
+    {
+        int a[] = {0, 0, 1, 2, 2, 4};
+        binary_search<int*> b(&a[0], &a[sizeof(a) / sizeof(int)]);
+        auto t = b;
+        EXPECT_EQ(b.upper_bound(0) - (int*)&a, 2, "Expect 2, not %u", (b = t).upper_bound(0) - (int*)&a);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.upper_bound(3) - (int*)&a, 5);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.upper_bound(-9) - (int*)&a, 0);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.upper_bound(4) - (int*)&a, 6);
+        EXPECT_FALSE(b);
+        b = t;
+
+        EXPECT_EQ(b.upper_bound(5) - (int*)&a, 6);
+        EXPECT_FALSE(b);
+        b = t;
+
+        EXPECT_EQ(b.upper_bound(5, true) - (int*)&a, 5);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.upper_bound(0, true) - (int*)&a, 1);
+        EXPECT_TRUE(b);
+    }
+
+    TEST(bound_pos)
+    {
+        int a[] = {0, 0, 1, 2, 2, 4};
+        binary_search<int*> b(&a[0], &a[sizeof(a) / sizeof(int)]);
+        auto t = b;
+
+        EXPECT_EQ(b.upper_bound_pos(0), 2, "Expect 2, not %u", (b = t).upper_bound_pos(0));
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.upper_bound_pos(3), 5);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.upper_bound_pos(-9), 0);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.upper_bound_pos(4), 6);
+        EXPECT_FALSE(b);
+        b = t;
+
+        EXPECT_EQ(b.lower_bound_pos(0), 0, "Expect 0, not %u", (b = t).lower_bound_pos(0));
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.lower_bound_pos(3), 5);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.lower_bound_pos(-9), 0);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.lower_bound_pos(4), 5);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.lower_bound_pos(5, true), 5);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.lower_bound_pos(0, true), -1);
+        EXPECT_FALSE(b);
+        b = t;
+
+        EXPECT_EQ(b.upper_bound_pos(5, true), 5);
+        EXPECT_TRUE(b);
+        b = t;
+
+        EXPECT_EQ(b.upper_bound_pos(0, true), 1);
+        EXPECT_TRUE(b);
+    }
+
+    TEST(bounds_check)
+    {
+        int a[] = {0, 0, 1, 2, 2, 4};
+        binary_search<int*> b(&a[0], &a[sizeof(a) / sizeof(int)]);
+        auto t = b;
+
+        auto r = b.upper_bound_pos_check(0);
+        EXPECT_EQ(r.first, 2, "Expect 2, not %u", r.first);
+        EXPECT_TRUE(r.second, "Expect true, not false");
+        b = t;
+
+        r = b.lower_bound_pos_check(0);
+        EXPECT_EQ(r.first, 0, "Expect 0, not %u", r.first);
+        EXPECT_TRUE(r.second, "Expect true, not false");
+        b = t;
+
+        r = b.upper_bound_pos_check(4);
+        EXPECT_EQ(r.first, 6, "Expect 6, not %u", r.first);
+        EXPECT_FALSE(r.second, "Expect false, not true");
+        b = t;
+
+        r = b.lower_bound_pos_check(5);
+        EXPECT_EQ(r.first, 6, "Expect 6, not %u", r.first);
+        EXPECT_FALSE(r.second, "Expect false, not true");
+        b = t;
+
+        auto r2 = b.upper_bound_check(0);
+        EXPECT_EQ(r2.first - (int*)&a, 2, "Expect &a[2], not &a[%u]", r2.first - (int*)&a);
+        EXPECT_TRUE(r2.second, "Expect true, not false");
+        b = t;
+
+        r2 = b.lower_bound_check(0);
+        EXPECT_EQ(r2.first - (int*)&a, 0, "Expect &a[0], not &a[%u]", r2.first);
+        EXPECT_TRUE(r2.second, "Expect true, not false");
+        b = t;
+
+        r2 = b.upper_bound_check(4);
+        EXPECT_EQ(r2.first - (int*)&a, 6, "Expect &a[6], not &a[%u]", r2.first);
+        EXPECT_FALSE(r2.second, "Expect false, not true");
+        b = t;
+
+        r2 = b.lower_bound_check(5);
+        EXPECT_EQ(r2.first - (int*)&a, 6, "Expect &a[6], not &a[%u]", r2.first);
+        EXPECT_FALSE(r2.second, "Expect false, not true");
+        b = t;
+    }
+
+    TEST(bounds)
+    {
+        int a[] = {0, 0, 1, 2, 2, 4};
+        binary_search<int*> b(&a[0], &a[sizeof(a) / sizeof(int)]);
+        auto t = b;
+
+        auto r = b.bounds(-1);
+        EXPECT_GT(r.first, r.second);
+        EXPECT_FALSE(b);
+        b = t;
+
+        r = b.bounds(0);
+        EXPECT_EQ(r.first - (int*)&a, 0);
+        EXPECT_EQ(r.second - (int*)&a, 1);
+        EXPECT_TRUE(b);
+        b = t;
+
+        r = b.bounds(3);
+        EXPECT_GT(r.first, r.second);
+        EXPECT_FALSE(b);
+        b = t;
+
+        r = b.bounds(4);
+        EXPECT_EQ(r.first - (int*)&a, 5);
+        EXPECT_EQ(r.second - (int*)&a, 5);
+        EXPECT_TRUE(b);
+        b = t;
+
+        r = b.bounds(5);
+        EXPECT_GT(r.first, r.second);
+        EXPECT_FALSE(b);
+        b = t;
+
+        auto r2 = b.bounds_pos(-1);
+        EXPECT_GT(r2.first, r2.second);
+        EXPECT_FALSE(b);
+        b = t;
+
+        r2 = b.bounds_pos(0);
+        EXPECT_EQ(r2.first, 0);
+        EXPECT_EQ(r2.second, 1);
+        EXPECT_TRUE(b);
+        b = t;
+
+        r2 = b.bounds_pos(3);
+        EXPECT_GT(r2.first, r2.second);
+        EXPECT_FALSE(b);
+        b = t;
+
+        r2 = b.bounds_pos(4);
+        EXPECT_EQ(r2.first, 5);
+        EXPECT_EQ(r2.second, 5);
+        EXPECT_TRUE(b);
+        b = t;
+
+        r2 = b.bounds_pos(5);
+        EXPECT_GT(r2.first, r2.second);
+        EXPECT_FALSE(b);
     }
 }
