c62afa8499996cca133d69e2236113d9dffe4e7f
Idolin <dk.1997-fast@yandex.ru>
2019-04-25 08:51:38 +0300

    Spinlock improvement

--- src/parallelism/spinlock.hpp
+++ src/parallelism/spinlock.hpp
@@ -9,10 +9,12 @@
 #include <type_traits>
 
 #include "../debug/def_debug.h"
-#include "flag_wait_algo.hpp"
 
-template<unsigned wait = 100>
-struct spinlock
+template<bool recursive=false, bool read_lock=false, bool queued = false>
+struct spinlock;
+
+template<>
+struct spinlock<false, false, false>
 {
 private:
     std::atomic_flag flag = ATOMIC_FLAG_INIT;
@@ -25,7 +27,8 @@ public:
 
     void lock()
     {
-        wait_for_flag<wait>(flag);
+        while(flag.test_and_set(std::memory_order_acquire))
+            std::this_thread::yield();
     }
 
     bool try_lock()
@@ -38,3 +41,56 @@ public:
         flag.clear(std::memory_order_release);
     }
 };
+
+template<>
+struct spinlock<false, false, true>
+{
+    struct q_node
+    {
+        std::atomic<q_node*> next;
+        std::atomic_flag flag;
+    };
+
+private:
+    std::atomic<q_node*> tail = {nullptr};
+
+public:
+    ~spinlock()
+    {
+        ASSERT_NO_THROW(tail == nullptr);
+    }
+
+    void lock()
+    {
+        node.next.store(nullptr, std::memory_order_acquire), node.flag.test_and_set(std::memory_order_acquire);
+        q_node* tmp = &node;
+        tmp = tail.exchange(tmp, std::memory_order_seq_cst);
+        if(tmp != nullptr)
+        {
+            tmp->next.store(&node, std::memory_order_seq_cst);
+            while(node.flag.test_and_set(std::memory_order_acquire))
+                std::this_thread::yield();
+        }
+    }
+
+    bool try_lock()
+    {
+        node.next.store(nullptr, std::memory_order_acquire), node.flag.test_and_set(std::memory_order_acquire);
+        q_node* tmp = &node;
+        return tail.compare_exchange_strong(tmp, nullptr, std::memory_order_seq_cst);
+    }
+
+    void unlock()
+    {
+        q_node* tmp = &node;
+        if(!tail.compare_exchange_strong(tmp, nullptr, std::memory_order_acquire))
+        {
+            while(node.next.load(std::memory_order_acquire) == nullptr);
+            node.next.load(std::memory_order_acquire)->flag.clear(std::memory_order_release);
+        }
+    }
+
+    thread_local static q_node node;
+};
+
+thread_local typename spinlock<false, false, true>::q_node spinlock<false, false, true>::node;
