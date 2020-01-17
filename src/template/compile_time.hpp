c3488710be8cc42b29454c4f25560b91abebfe77
Idolin <dk.1997-fast@yandex.ru>
2020-01-17 21:16:17 +0300

    moved to usage of std::array and std::function

--- src/template/compile_time.hpp
+++ src/template/compile_time.hpp
@@ -1,23 +1,22 @@
 #pragma once
 
 #include <array>
+#include <functional>
 #include <type_traits>
 
 #include "commonmethods.hpp"
 
-template<typename T, typename SizeType, size_t... i>
-static constexpr auto _make_array(T(*func)(SizeType index), std::index_sequence<i...>)
+template<typename T, size_t... i>
+static constexpr auto _make_array(std::function<T(std::size_t index)> func, std::index_sequence<i...>)
 {
     return std::array<T, sizeof...(i)>{{func(i)...}};
 }
 
-template<typename T, size_t array_size, typename SizeType = unsigned,
-    typename = typename std::enable_if_t<std::is_integral<SizeType>::value>>
-constexpr std::array<T, array_size> get_compile_time_array(T(*func)(SizeType index))
+template<typename T, size_t array_size>
+constexpr std::array<T, array_size> get_compile_time_array(std::function<T(std::size_t index)> func)
 {
-    return _make_array<T, SizeType>(func, std::make_index_sequence<array_size>{});
+    return _make_array<T>(func, std::make_index_sequence<array_size>{});
 }
 
-template<typename T, size_t array_size, typename SizeType, T (*func)(SizeType),
-    typename = typename std::enable_if_t<std::is_integral<SizeType>::value>>
-constexpr std::array<T, array_size> compile_time_array = get_compile_time_array<T, array_size, SizeType>(func);
+template<typename T, size_t array_size, T(*func)(std::size_t index)>
+constexpr std::array<T, array_size> compile_time_array = get_compile_time_array<T, array_size>(func);
