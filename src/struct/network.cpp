c5bcadd1a559d9b34752bdf5dbe6392791589b99
Idolin <dk.1997-fast@yandex.ru>
2020-03-17 17:56:33 +0300

    Updated to use current methods

--- src/struct/network.cpp
+++ src/struct/network.cpp
@@ -1,9 +1,15 @@
+#include "../container/vector.hpp"
+#include "../container/queue.hpp"
+
 struct network
 {
     struct nTransition
     {
         unsigned to, reversed;
         int flow, capacity;
+
+        nTransition()
+        {}
 
         nTransition(unsigned to, unsigned capacity, unsigned reversed) : to(to), reversed(reversed), flow(0),
                                                                          capacity(capacity)
@@ -16,26 +22,26 @@ struct network
     bool *f;
     unsigned sink;
 
-    network(unsigned n) : nLen(n), edge(new vect<nTransition>[n]), f(new bool[n]), pNextEdge(new unsigned[n])
+    network(unsigned n) : nLen(n), edge(new vect<nTransition>[n]), pNextEdge(new unsigned[n]), f(new bool[n])
     {}
 
     void addEdge(unsigned from, unsigned to, unsigned flow)
     {
-        edge[from].push(nTransition(to, flow, edge[to].maxs));
-        edge[to].push(nTransition(from, 0, edge[from].maxs - 1));
+        edge[from].push(nTransition(to, flow, edge[to].size()));
+        edge[to].push(nTransition(from, 0, edge[from].size() - 1));
     }
 
     bool bfs(unsigned s)
     {
         _fill(f, nLen);
-        queue q;
+        queue<unsigned> q;
         rPos[s] = 0;
         f[s] = true;
         q.push(s);
-        while(q.length)
+        while(q.size())
         {
             unsigned v = q.pop();
-            for(unsigned i = 0; i < edge[v].maxs; i++)
+            for(unsigned i = 0; i < edge[v].size(); i++)
             {
                 nTransition e = edge[v][i];
                 if(!f[e.to] && (e.flow < e.capacity))
@@ -49,18 +55,18 @@ struct network
         return f[sink];
     }
 
-    unsigned dfs(unsigned s, unsigned maxPossible = UINT_MAX)
+    unsigned dfs(unsigned s, unsigned maxPossible = std::numeric_limits<unsigned>::max())
     {
         if((s == sink) || (maxPossible == 0))
             return maxPossible;
-        for(unsigned i = pNextEdge[s]; i < edge[s].maxs; i++)
+        for(unsigned i = pNextEdge[s]; i < edge[s].size(); i++)
         {
             if(rPos[s] + 1 == rPos[edge[s][i].to])
             {
                 unsigned flowLeft = edge[s][i].capacity - edge[s][i].flow;
                 if(flowLeft)
                 {
-                    unsigned flow = dfs(edge[s][i].to, min(maxPossible, flowLeft));
+                    unsigned flow = dfs(edge[s][i].to, std::min(maxPossible, flowLeft));
                     if(flow)
                     {
                         edge[s][i].flow += flow;
