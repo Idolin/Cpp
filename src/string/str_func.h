297b4ce8acf10815c6e03b4903b95602b780f789
Idolin <dk.1997-fast@yandex.ru>
2020-05-20 21:08:47 +0300

    Taking template arrays as arguments instead of str

--- src/string/str_func.h
+++ src/string/str_func.h
@@ -3,17 +3,6 @@
 #include "../container/vector.hpp"
 #include "str.h"
 
-unsigned *pi(const str &);
-
-unsigned *z(const str &);
-
-unsigned find(const str &, const str &, const unsigned *const);
-
-inline unsigned find(const str &s, const str &sub)
-{
-    return find(s, sub, pi(sub));
-}
-
 inline bool isSubstr(const str &s, const str &sub, const unsigned *const pi_sub)
 {
     return (find(s, sub, pi_sub));
@@ -23,17 +12,6 @@ inline bool isSubstr(const str &s, const str &sub)
 {
     return (find(s, sub));
 }
-
-vect<unsigned> findAll(const str &, const str &, const unsigned *const);
-
-inline vect<unsigned> findAll(const str &s, const str &sub)
-{
-    return findAll(s, sub, pi(sub));
-}
-
-vect<unsigned> findAllClose(const str &, const str &, unsigned, const unsigned *const);
-
-vect<unsigned> findAllClose(const str &, const str &, unsigned);
 
 inline unsigned getPeriod(const unsigned *const pi, unsigned len)
 {
