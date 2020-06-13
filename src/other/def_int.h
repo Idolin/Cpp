265404119ca67ef53755fb9aff6e947ec8911309
Idolin <dk.1997-fast@yandex.ru>
2020-06-13 17:14:55 +0300

    Macros internal implementation moved into def_int.h

---
+++ src/other/def_int.h
@@ -0,0 +1,290 @@
+#pragma once
+
+/* WARNING: DARK MACROMAGIC */
+
+// prefix D_INTR_ is used for internal macros 
+
+#define D_INTR_TAKE53(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, \
+                _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, \
+                _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, ...) _53
+
+#define D_INTR_SELF(...) __VA_ARGS__
+#define LAST_D_INTR_SELF(...) __VA_ARGS__
+
+#define D_INTR_CE_(...) , ## __VA_ARGS__
+#define D_INTR_CE(...) D_INTR_CE_(__VA_ARGS__) // put comma before non-empty list of arguments
+#define D_INTR_CE_I_(...) , ## __VA_ARGS__
+#define D_INTR_CE_I(...) D_INTR_CE_I_(__VA_ARGS__) // put comma before non-empty list of arguments
+
+#define D_INTR_MS_(...) __VA_ARGS__
+#define D_INTR_MS(...) D_INTR_MS_(__VA_ARGS__) // another SELF macros
+
+#define D_INTR_MSS(...) __VA_ARGS__  // just another SELF macros
+
+#define D_INTR_LSELF(...) __VA_ARGS__
+#define LAST_D_INTR_LSELF(...)
+
+#define D_INTR_CALL(f, arg) f arg // call another macro function where arg is (args...)
+
+// returns 0 on zero amount of arguments and 1 otherwise
+#define D_INTR_GET_ARGS_COUNT_2(...) D_INTR_SELF(D_INTR_TAKE53 D_INTR_SELF() (1, ## __VA_ARGS__, \
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, \
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, \
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0))
+
+// if amount of arguments is zero returns 0,
+// if 1 then returns 1, otherwise returns 2
+#define D_INTR_GET_ARGS_COUNT_3(...) D_INTR_SELF(D_INTR_TAKE53 D_INTR_SELF() (2, ## __VA_ARGS__, \
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, \
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, \
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0))
+
+#define D_INTR_GET_ARG_DEF_0() NO_SUCH_ARG
+#define D_INTR_GET_ARG_DEF_1(a) a
+#define D_INTR_GET_ARG_DEF_2(a, ...) __VA_ARGS__
+
+#define D_INTR_START_IF_ARGS_NDEF_0(...)
+#define D_INTR_START_IF_ARGS_NDEF_1(f, ...) f(__VA_ARGS__)
+#define D_INTR_START_IF_ARGS_NDEF_2(f, ...) D_INTR_START_IF_ARGS_NDEF_1(f, ## __VA_ARGS__)
+
+#define D_INTR_START_IF_ARGS_0(f)
+#define D_INTR_START_IF_ARGS_1(f, ...) f(__VA_ARGS__)
+
+#define D_INTR_MC_(a, b) a ## b
+#define D_INTR_MC(a, b) D_INTR_MC_(a, b) // another CONCAT macros
+
+#define D_INTR_CEE_0 D_INTR_MS
+#define D_INTR_CEE_1 D_INTR_CE
+
+// returns D_INTR_CE on zero amount of arguments or D_INTR_MS otherwise
+#define D_INTR_CEE(...) D_INTR_MSS(__VA_ARGS__)D_INTR_MC(D_INTR_CEE_, D_INTR_GET_ARGS_COUNT_2(__VA_ARGS__))
+
+// returns amount of arguments in argument list
+#define D_INTR_GET_ARGS_COUNT(...) D_INTR_TAKE53(53, 52, ## __VA_ARGS__, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, \
+                                                24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+
+#define D_INTR_CONCAT_(a, b) a ## b
+#define D_INTR_CONCAT(a, b) D_INTR_CONCAT_(a, b)
+#define D_INTR_CONCAT_I_(a, b) a ## b
+#define D_INTR_CONCAT_I(a, b) D_INTR_CONCAT_I_(a, b)
+    
+#define D_INTR_EMPTY(...)
+
+#define D_INTR_QUOTE(...) # __VA_ARGS__
+
+#define D_INTR_FREE_1(...) __VA_ARGS__
+#define D_INTR_FREE_0(X) D_INTR_SELF X
+
+#define D_INTR_FCONCAT_(E, ...) E __VA_ARGS__
+#define D_INTR_FCONCAT(...) D_INTR_FCONCAT_(D_INTR_EMPTY, ## __VA_ARGS__)
+#define D_INTR_F_CALL(f, arg) f arg // another CALL
+
+// if argument is in braces than removes outer braces, otherwise returns arguments intact
+#define D_INTR_FREE(...) D_INTR_CONCAT(D_INTR_FREE_, D_INTR_F_CALL(D_INTR_GET_ARGS_COUNT_2, (D_INTR_FCONCAT(__VA_ARGS__))))(__VA_ARGS__)
+    
+#define D_INTR_LSKIP_1(a, ...) __VA_ARGS__
+#define LAST_D_INTR_LSKIP_1(a, ...) a
+    
+#define D_INTR_SKIP_1(a, ...) __VA_ARGS__
+#define LAST_D_INTR_SKIP_1(...) D_INTR_SKIP_1(__VA_ARGS__)
+
+#define D_INTR_GET_3(a, b, c, ...) a, b, c
+#define LAST_D_INTR_GET_3(...) GET_3(__VA_ARGS__)
+
+#define D_INTR_SKIP_1_3(a, b, skip, ...) a, b, ## __VA_ARGS__
+#define LAST_D_INTR_SKIP_1_3(...) SKIP_1_3(__VA_ARGS__)
+    
+#define D_INTR_GET_FIRST(a, ...) a
+
+#define D_INTR_POW_10(a, ...) a ## 0, a, ## __VA_ARGS__
+#define LAST_D_INTR_POW_10() 1
+
+#define D_INTR_MULT_ARG_0(...)
+#define D_INTR_MULT_ARG_1(...) __VA_ARGS__
+#define D_INTR_MULT_ARG_2(...) D_INTR_MULT_ARG_1(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_3(...) D_INTR_MULT_ARG_2(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_4(...) D_INTR_MULT_ARG_3(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_5(...) D_INTR_MULT_ARG_4(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_6(...) D_INTR_MULT_ARG_5(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_7(...) D_INTR_MULT_ARG_6(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_8(...) D_INTR_MULT_ARG_7(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_9(...) D_INTR_MULT_ARG_8(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_10(...) D_INTR_MULT_ARG_9(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_11(...) D_INTR_MULT_ARG_10(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_12(...) D_INTR_MULT_ARG_11(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_13(...) D_INTR_MULT_ARG_12(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_14(...) D_INTR_MULT_ARG_13(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_15(...) D_INTR_MULT_ARG_14(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_16(...) D_INTR_MULT_ARG_15(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_17(...) D_INTR_MULT_ARG_16(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_18(...) D_INTR_MULT_ARG_17(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_19(...) D_INTR_MULT_ARG_18(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_20(...) D_INTR_MULT_ARG_19(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_21(...) D_INTR_MULT_ARG_20(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_22(...) D_INTR_MULT_ARG_21(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_23(...) D_INTR_MULT_ARG_22(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_24(...) D_INTR_MULT_ARG_23(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_25(...) D_INTR_MULT_ARG_24(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_26(...) D_INTR_MULT_ARG_25(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_27(...) D_INTR_MULT_ARG_26(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_28(...) D_INTR_MULT_ARG_27(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_29(...) D_INTR_MULT_ARG_28(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_30(...) D_INTR_MULT_ARG_29(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_31(...) D_INTR_MULT_ARG_30(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_32(...) D_INTR_MULT_ARG_31(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_33(...) D_INTR_MULT_ARG_32(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_34(...) D_INTR_MULT_ARG_33(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_35(...) D_INTR_MULT_ARG_34(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_36(...) D_INTR_MULT_ARG_35(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_37(...) D_INTR_MULT_ARG_36(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_38(...) D_INTR_MULT_ARG_37(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_39(...) D_INTR_MULT_ARG_38(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_40(...) D_INTR_MULT_ARG_39(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_41(...) D_INTR_MULT_ARG_40(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_42(...) D_INTR_MULT_ARG_41(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_43(...) D_INTR_MULT_ARG_42(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_44(...) D_INTR_MULT_ARG_43(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_45(...) D_INTR_MULT_ARG_44(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_46(...) D_INTR_MULT_ARG_45(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_47(...) D_INTR_MULT_ARG_46(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_48(...) D_INTR_MULT_ARG_47(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_49(...) D_INTR_MULT_ARG_48(__VA_ARGS__), ## __VA_ARGS__
+#define D_INTR_MULT_ARG_50(...) D_INTR_MULT_ARG_49(__VA_ARGS__), ## __VA_ARGS__
+
+#define D_INTR_MULT_ARG_N(N, ...) D_INTR_CONCAT_I(D_INTR_MULT_ARG_, N)(__VA_ARGS__)
+
+// returns 50 - N
+#define D_INTR_50MN(N) D_INTR_SELF(D_INTR_TAKE53 D_INTR_SELF() (<?>, <?>, <?> D_INTR_CE(D_INTR_MULT_ARG_N(N, 0)), \
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, \
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, \
+    28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, \
+    45, 46, 47, 48, 49, 50))
+
+// arguments multiplied 50 - N times
+#define D_INTR_MULT_ARG_50_MN(N, ...) \
+    D_INTR_MULT_ARG_N(D_INTR_50MN(N), ## __VA_ARGS__)
+
+// returns amount of arguments - N
+#define D_INTR_GET_ARGS_COUNT_MN(N, ...) D_INTR_SELF(D_INTR_TAKE53 D_INTR_SELF() (1, ## __VA_ARGS__, D_INTR_MULT_ARG_50_MN(N, 1), 1, 0, 0, 0))
+    
+// returns Nth argument from given argument list
+#define D_INTR_GET_ARG_N(N, ...) \
+    D_INTR_CONCAT(D_INTR_MULT_ARG_N_, N)(D_INTR_LSKIP_1, D_INTR_GET_FIRST, D_INTR_SKIP_1, ## __VA_ARGS__)
+
+// returns last argument from list
+#define D_INTR_GET_LAST(...) GET_ARG_N(D_INTR_GET_ARGS_COUNT(__VA_ARGS__), __VA_ARGS__)
+
+// D_INTR_CEE shouldn't influence on macro expansion except for the extra space after commas
+#define MULT_ARG_R_0(C, G, N, ...)
+#define MULT_ARG_R_1(C, G, N, ...) D_INTR_CALL(LAST_ ## C, (__VA_ARGS__))
+#define MULT_ARG_R_2(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_1(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_3(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_2(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_4(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_3(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_5(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_4(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_6(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_5(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_7(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_6(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_8(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_7(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_9(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_8(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_10(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_9(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_11(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_10(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_12(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_11(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_13(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_12(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_14(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_13(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_15(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_14(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_16(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_15(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_17(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_16(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_18(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_17(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_19(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_18(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_20(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_19(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_21(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_20(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_22(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_21(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_23(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_22(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_24(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_23(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_25(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_24(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_26(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_25(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_27(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_26(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_28(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_27(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_29(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_28(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_30(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_29(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_31(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_30(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_32(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_31(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_33(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_32(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_34(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_33(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_35(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_34(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_36(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_35(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_37(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_36(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_38(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_37(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_39(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_38(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_40(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_39(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_41(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_40(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_42(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_41(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_43(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_42(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_44(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_43(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_45(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_44(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_46(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_45(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_47(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_46(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_48(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_47(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_49(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_48(C, G, N, N(__VA_ARGS__)))))
+#define MULT_ARG_R_50(C, G, N, ...) \
+    D_INTR_CALL(C,(D_INTR_CEE(G(__VA_ARGS__))(MULT_ARG_R_49(C, G, N, N(__VA_ARGS__)))))
+    
+#define D_INTR_COMPOSE_FOR_EACH(B, A, X, ...) { B X D_INTR_FREE(A) } __VA_ARGS__
+#define LAST_D_INTR_COMPOSE_FOR_EACH(B, A, X) { B X D_INTR_FREE(A) }
+    
+#define D_INTR_REV_COMPOSE(a, ...) __VA_ARGS__, a
+#define LAST_D_INTR_REV_COMPOSE(a) a
+    
+#define D_INTR_COMPOSE_SEQUENCE(init, operation, sequence, ...) (sequence operation), sequence, ## __VA_ARGS__
+#define LAST_D_INTR_COMPOSE_SEQUENCE(init, operation) (init)
+    
+// returns arguments list in reversed order
+#define D_INTR_REVERSE(...) \
+    MULT_ARG_R_N(D_INTR_GET_ARGS_COUNT(__VA_ARGS__), D_INTR_REV_COMPOSE, D_INTR_GET_FIRST, D_INTR_SKIP_1, ## __VA_ARGS__)
