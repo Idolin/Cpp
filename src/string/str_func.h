da6003bb45bab4c018442f407248c9e4070b55a0
Idolin <dk.1997-fast@yandex.ru>
2021-02-04 00:08:15 +0300

    Changing argument type to cstr from str

--- src/string/str_func.h
+++ src/string/str_func.h
@@ -7,7 +7,7 @@ inline unsigned getPeriod(const unsigned *const pi, unsigned len)
     return len - pi[len - 1];
 }
 
-inline unsigned getPeriod(const str &s)
+inline unsigned getPeriod(const cstr &s)
 {
     return getPeriod(pi(s), s.length());
 }
