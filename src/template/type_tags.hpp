323c5adea496d146d4915802475b5f6ebbc1add1
Idolin <dk.1997-fast@yandex.ru>
2021-09-17 02:17:36 +0300

    Added missing <utility> header
    Added few more type checks

--- src/template/type_tags.hpp
+++ src/template/type_tags.hpp
@@ -4,6 +4,7 @@
 
 #include <cstdint>
 #include <type_traits>
+#include <utility>
 
 template<typename T>
 struct is_numeric
@@ -82,9 +83,9 @@ struct is_mergeable
 };
 
 
-#define STRUCT_IS_T(name, ...) \
+#define IMPL_TYPE_TAGS_STRUCT_CHECK_T(name, ...) \
     template<typename T> \
-    struct is_ ## name<T, typename std::enable_if_t<__VA_ARGS__>> \
+    struct name<T, typename std::enable_if_t<__VA_ARGS__>> \
     { \
         enum \
         { \
@@ -92,9 +93,9 @@ struct is_mergeable
         }; \
     };
 
-#define STRUCT_IS(name, ...) \
+#define IMPL_TYPE_TAGS_STRUCT_CHECK(name, ...) \
     template<typename T, typename Enable = void> \
-    struct is_ ## name \
+    struct name \
     { \
         enum \
         { \
@@ -102,37 +103,59 @@ struct is_mergeable
         }; \
     }; \
     \
-    STRUCT_IS_T(name, ## __VA_ARGS__)
+    IMPL_TYPE_TAGS_STRUCT_CHECK_T(name, ## __VA_ARGS__) \
+    \
+    template<typename T> \
+    constexpr bool name ## _v = name<T>::value;
 
-STRUCT_IS(less_comparable, std::is_same<
+
+#define IMPL_TYPE_TAGS_STRUCT_IS_T(name, ...) IMPL_TYPE_TAGS_STRUCT_CHECK_T(is_ ## name, __VA_ARGS__)
+#define IMPL_TYPE_TAGS_STRUCT_IS(name, ...) IMPL_TYPE_TAGS_STRUCT_CHECK(is_ ## name, __VA_ARGS__)
+
+#define IMPL_TYPE_TAGS_STRUCT_HAS_T(name, ...) IMPL_TYPE_TAGS_STRUCT_CHECK_T(has_ ## name, ## __VA_ARGS__)
+#define IMPL_TYPE_TAGS_STRUCT_HAS(name, ...) IMPL_TYPE_TAGS_STRUCT_CHECK(has_ ## name, ## __VA_ARGS__)
+
+
+IMPL_TYPE_TAGS_STRUCT_IS(less_comparable, std::is_same<
         decltype(std::declval<typename std::add_lvalue_reference<typename std::add_const<T>::type>::type>() <
          std::declval<typename std::add_lvalue_reference<typename std::add_const<T>::type>::type>()), bool>::value)
 
-STRUCT_IS(dereferencable, std::is_lvalue_reference<
+IMPL_TYPE_TAGS_STRUCT_IS(dereferencable, std::is_lvalue_reference<
         decltype(*std::declval<typename std::add_lvalue_reference<T>::type>())>::value)
 
+IMPL_TYPE_TAGS_STRUCT_HAS(arrow_operator, std::is_pointer<decltype(std::declval<T>().operator->())>::value)
 
-STRUCT_IS(hashable, std::is_integral<T>::value || std::is_pointer<T>::value || is_cstr<T>::value)
+IMPL_TYPE_TAGS_STRUCT_HAS(equality_operator, std::is_convertible<decltype(std::declval<T>() == std::declval<T>()), bool>::value)
 
-STRUCT_IS_T(hashable, std::is_same<
+IMPL_TYPE_TAGS_STRUCT_HAS(inequality_operator, std::is_convertible<decltype(std::declval<T>() != std::declval<T>()), bool>::value)
+
+IMPL_TYPE_TAGS_STRUCT_HAS(pre_increment_operator,
+        std::is_same<decltype(++std::declval<T>()), std::add_lvalue_reference_t<T>>::value)
+
+IMPL_TYPE_TAGS_STRUCT_HAS(post_increment_operator,
+        std::is_convertible<decltype(std::declval<T>()++), T>::value)
+
+IMPL_TYPE_TAGS_STRUCT_IS(hashable, std::is_integral<T>::value || std::is_pointer<T>::value || is_cstr<T>::value)
+
+IMPL_TYPE_TAGS_STRUCT_IS_T(hashable, std::is_same<
         decltype(std::declval<typename std::add_lvalue_reference<
                 typename std::add_const<T>::type>::type>().hash()), uint64_t>::value)
 
-STRUCT_IS(block_splittable, std::is_integral<T>::value || std::is_pointer<T>::value)
+IMPL_TYPE_TAGS_STRUCT_IS(block_splittable, std::is_integral<T>::value || std::is_pointer<T>::value)
 
-STRUCT_IS_T(block_splittable, std::is_same<decltype(std::declval<
+IMPL_TYPE_TAGS_STRUCT_IS_T(block_splittable, std::is_same<decltype(std::declval<
              typename std::add_lvalue_reference<typename std::add_const<T>::type>::type>().size()), std::size_t>::value &&
         is_block_splittable<decltype(std::declval<
                 typename std::add_lvalue_reference<
                         typename std::add_const<T>::type>::type>().operator[][static_cast<std::size_t>(0)])>::value)
 
-STRUCT_IS(block_iterable, is_block_splittable<T>::value)
+IMPL_TYPE_TAGS_STRUCT_IS(block_iterable, is_block_splittable<T>::value)
 
-STRUCT_IS(block_iterable_class, std::is_same<
+IMPL_TYPE_TAGS_STRUCT_IS(block_iterable_class, std::is_same<
         decltype(std::declval<typename std::add_lvalue_reference<typename std::add_const<T>::type>::type>().cbegin()),
         decltype(std::declval<typename std::add_lvalue_reference<
                 typename std::add_const<T>::type>::type>().cend())>::value &&
     is_block_iterable<decltype(*(std::declval<typename std::add_lvalue_reference<
             typename std::add_const<T>::type>::type>().cbegin()))>::value)
 
-STRUCT_IS_T(block_iterable, is_block_iterable_class<T>::value)
+IMPL_TYPE_TAGS_STRUCT_IS_T(block_iterable, is_block_iterable_class<T>::value)
