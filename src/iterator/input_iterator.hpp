8e7b369e7d6dd3c29e2844ef0133fb1113d5bc95
Idolin <dk.1997-fast@yandex.ru>
2021-09-17 23:01:13 +0300

    Made constructor perfect forwarding
    Added comments

--- src/iterator/input_iterator.hpp
+++ src/iterator/input_iterator.hpp
@@ -3,6 +3,7 @@
 #include "./iterator_impl.hpp"
 
 #include <iterator>
+#include <utility>
 
 namespace iterator_impl_def {
 
@@ -11,8 +12,16 @@ namespace iterator_impl_def {
              typename pointer_t,
              typename iterator_t>
     struct _input_iterator_adapter: Impl,
+            // adds operator!= if not declared based on operator==
             iterator_impl_def::_notEqualDefault<Impl, iterator_t>,
+            // adds operator-> if not declared based on operator*
             iterator_impl_def::_arrowDefault<Impl, iterator_t, pointer_t>,
+            /*
+             * adds pre-increment operator++() if not declared based on increment() method
+             * adds post-increment operator++(int) if not declared based on either
+             *  - pre-increment operator++()
+             *  - increment() method
+             */
             iterator_impl_def::_incrementDefault<Impl, iterator_t>
     {
         typedef std::input_iterator_tag iterator_category;
@@ -21,10 +30,15 @@ namespace iterator_impl_def {
         typedef pointer_t pointer;
         typedef void difference_type;
 
+        // forward constructor
         template<typename... Types>
-        _input_iterator_adapter(Types... values): Impl(values...)
+        _input_iterator_adapter(Types&&... values): Impl(std::forward<Types>(values)...)
         {}
 
+        /*
+         * Increment operators implemented here, in upper score,
+         *  to ensure they will be called instead of original ones declared in Impl class
+         */
         iterator_t& operator++()
         {
             iterator_impl_def::_incrementDefault<Impl, iterator_t>::_preIncrement();
@@ -47,10 +61,11 @@ template<class Impl, typename value_type_t = typename iterator_impl_def::get_def
 struct input_iterator: iterator_impl_def::_input_iterator_adapter<Impl, value_type_t, reference_t, pointer_t,
         input_iterator<Impl, value_type_t, reference_t, pointer_t>>
 {
+    // forward constructor
     template<typename... Types>
-    input_iterator(Types... values):
+    input_iterator(Types&&... values):
         iterator_impl_def::_input_iterator_adapter<Impl, value_type_t, reference_t, pointer_t,
-            input_iterator<Impl, value_type_t, reference_t, pointer_t>>(values...)
+            input_iterator<Impl, value_type_t, reference_t, pointer_t>>(std::forward<Types>(values)...)
     {}
 };
 
