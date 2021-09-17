492b4c585e10d38df977bf357e7c0e62b8328446
Idolin <dk.1997-fast@yandex.ru>
2021-09-17 22:45:11 +0300

    Made iterator helper class concept to actually work

--- src/iterator/input_iterator.hpp
+++ src/iterator/input_iterator.hpp
@@ -4,17 +4,53 @@
 
 #include <iterator>
 
+namespace iterator_impl_def {
+
+    template<class Impl, typename value_type_t,
+             typename reference_t,
+             typename pointer_t,
+             typename iterator_t>
+    struct _input_iterator_adapter: Impl,
+            iterator_impl_def::_notEqualDefault<Impl, iterator_t>,
+            iterator_impl_def::_arrowDefault<Impl, iterator_t, pointer_t>,
+            iterator_impl_def::_incrementDefault<Impl, iterator_t>
+    {
+        typedef std::input_iterator_tag iterator_category;
+        typedef value_type_t value_type;
+        typedef reference_t reference;
+        typedef pointer_t pointer;
+        typedef void difference_type;
+
+        template<typename... Types>
+        _input_iterator_adapter(Types... values): Impl(values...)
+        {}
+
+        iterator_t& operator++()
+        {
+            iterator_impl_def::_incrementDefault<Impl, iterator_t>::_preIncrement();
+            return *static_cast<iterator_t*>(this);
+        }
+
+        iterator_t operator++(int)
+        {
+            iterator_t copy = *static_cast<iterator_t*>(this);
+            iterator_impl_def::_incrementDefault<Impl, iterator_t>::_postIncrement();
+            return copy;
+        }
+    };
+
+}
+
 template<class Impl, typename value_type_t = typename iterator_impl_def::get_def_value_t<Impl>,
          typename reference_t = typename iterator_impl_def::get_def_reference_t<Impl>,
          typename pointer_t = typename iterator_impl_def::get_def_pointer_t<Impl>>
-struct input_iterator: iterator_impl_def::_notEqualDefault<Impl>, iterator_impl_def::_arrowDefault<Impl, pointer_t>,
-        iterator_impl_def::_postIncrementDefault<Impl>
+struct input_iterator: iterator_impl_def::_input_iterator_adapter<Impl, value_type_t, reference_t, pointer_t,
+        input_iterator<Impl, value_type_t, reference_t, pointer_t>>
 {
-    typedef std::input_iterator_tag iterator_category;
-    typedef value_type_t value_type;
-    typedef reference_t reference;
-    typedef pointer_t pointer;
-    typedef void difference_type;
-
-    input_iterator() = default;
+    template<typename... Types>
+    input_iterator(Types... values):
+        iterator_impl_def::_input_iterator_adapter<Impl, value_type_t, reference_t, pointer_t,
+            input_iterator<Impl, value_type_t, reference_t, pointer_t>>(values...)
+    {}
 };
+
--- src/iterator/iterator_impl.hpp
+++ src/iterator/iterator_impl.hpp
@@ -12,24 +12,32 @@ namespace iterator_impl_def
         template<typename It>
         struct get_def_value
         {
+            static_assert(is_dereferencable_v<It>, "Iterator must be dereferencable");
+
             typedef std::remove_reference_t<decltype(*std::declval<It>())> type;
         };
 
         template<typename It>
         struct get_def_reference
         {
+            static_assert(is_dereferencable_v<It>, "Iterator must be dereferencable");
+
             typedef decltype(*std::declval<It>()) type;
         };
 
         template<typename It, typename Enable = void>
         struct get_def_pointer
         {
+            static_assert(is_dereferencable_v<It>, "Iterator must be dereferencable");
+
             typedef std::add_pointer_t<std::remove_reference_t<decltype(*std::declval<It>())>> type;
         };
 
         template<typename It>
         struct get_def_pointer<It, typename std::enable_if_t<has_arrow_operator_v<It>>>
         {
+            static_assert(is_dereferencable_v<It>, "Iterator must be dereferencable");
+
             typedef decltype(std::declval<It>().operator->()) type;
         };
     }
@@ -44,58 +52,126 @@ namespace iterator_impl_def
     using get_def_pointer_t = typename get_def_pointer<It>::type;
 
 
-    template<typename It, typename Enable = void>
+    template<typename Impl, typename It, typename Enable = void>
     struct _notEqualDefault
     {
-        static_assert(has_equality_operator_v<It>, "Iterator must have bool operator==");
+        static_assert(has_equality_operator_v<Impl>, "Iterator must have bool operator==");
 
         bool operator!=(const It& otr)
         {
-            return !(*static_cast<It*>(this) == otr);
+            return !((*static_cast<It*>(this)).Impl::operator==(otr));
         }
     };
 
-    template<typename It>
-    struct _notEqualDefault<It, typename std::enable_if_t<has_inequality_operator_v<It>>>
+    template<typename Impl, typename It>
+    struct _notEqualDefault<Impl, It, typename std::enable_if_t<has_inequality_operator_v<Impl>>>
     {
-        static_assert(has_equality_operator_v<It>, "Iterator must have bool operator==");
+        static_assert(has_equality_operator_v<Impl>, "Iterator must have bool operator==");
     };
 
 
-    template<typename It, typename R, typename Enable = void>
+    template<typename Impl, typename It, typename R, typename Enable = void>
     struct _arrowDefault
     {
         static_assert(is_dereferencable_v<R>, "Iterator pointer type better be dereferencable");
 
         R operator->()
         {
-            return &(static_cast<It*>(this)->operator*());
+            return &((*static_cast<It*>(this)).Impl::operator*());
         }
     };
 
-    template<typename It, typename R>
-    struct _arrowDefault<It, R, typename std::enable_if_t<has_arrow_operator_v<It>>>
+    template<typename Impl, typename It, typename R>
+    struct _arrowDefault<Impl, It, R, typename std::enable_if_t<has_arrow_operator_v<Impl>>>
     {
         static_assert(is_dereferencable_v<R>, "Iterator pointer type better be dereferencable");
     };
 
 
-    template<typename It, typename Enable = void>
+    template<typename Impl, typename It, typename Enable = void>
     struct _postIncrementDefault
     {
-        static_assert(has_pre_increment_operator_v<It>, "Iterator must have pre-increment operator");
+        static_assert(!has_pre_increment_operator_v<Impl> || !has_increment_method_v<Impl>,
+            "Iterator must have either pre-increment operator or increment method");
 
-        It operator++(int)
+        void _postIncrement()
         {
-            It copy = *static_cast<It*>(this);
-            ++*static_cast<It*>(this);
+            (*static_cast<It*>(this)).Impl::operator++();
+        }
+    };
+
+    template<typename Impl, typename It>
+    struct _postIncrementDefault<Impl, It, typename std::enable_if_t<has_increment_method_v<Impl> && !has_post_increment_operator_v<Impl>>>
+    {
+        void _postIncrement()
+        {
+            (*static_cast<It*>(this)).Impl::increment();
+        }
+    };
+
+    template<typename Impl, typename It>
+    struct _postIncrementDefault<Impl, It, typename std::enable_if_t<has_post_increment_operator_v<Impl>>>
+    {
+        static_assert(!has_post_increment_operator_v<Impl> || !has_increment_method_v<Impl>,
+            "Iterator must have either post-increment operator or increment method, not both");
+
+        void _postIncrement()
+        {
+            (*static_cast<It*>(this)).Impl::operator++(0);
+        }
+    };
+
+
+    template<typename Impl, typename It, bool has_pre_increment>
+    struct _preIncrementDefault;
+
+    template<typename Impl, typename It>
+    struct _preIncrementDefault<Impl, It, false>
+    {
+        static_assert(!has_pre_increment_operator_v<Impl> || !has_increment_method_v<Impl>,
+            "Iterator must have either pre-increment operator or increment method");
+
+        void _preIncrement()
+        {
+            (*static_cast<It*>(this)).Impl::increment();
+        }
+    };
+
+    template<typename Impl, typename It>
+    struct _preIncrementDefault<Impl, It, true>
+    {
+        static_assert(!has_pre_increment_operator_v<Impl> || !has_increment_method_v<Impl>,
+            "Iterator must have either pre-increment operator or increment method, not both");
+
+        void _preIncrement()
+        {
+           (*static_cast<It*>(this)).Impl::operator++();
+        }
+    };
+
+
+    template<typename Impl, typename It>
+    struct _incrementDefault: _preIncrementDefault<Impl, It, has_pre_increment_operator_v<Impl>>,
+            _postIncrementDefault<Impl, It>
+    {};
+
+
+    template<typename It, typename R, typename Enable = void>
+    struct _postDecrementDefault
+    {
+        static_assert(has_pre_decrement_operator_v<It>, "Iterator must have pre-decrement operator");
+
+        R operator--(int)
+        {
+            R copy = *static_cast<R*>(this);
+            static_cast<It*>(this)->decrement();
             return copy;
         }
     };
 
-    template<typename It>
-    struct _postIncrementDefault<It, typename std::enable_if_t<has_inequality_operator_v<It>>>
+    template<typename It, typename R>
+    struct _postDecrementDefault<It, R, typename std::enable_if_t<has_post_decrement_operator_v<It>>>
     {
-        static_assert(has_pre_increment_operator_v<It>, "Iterator must have pre-increment operator");
+        static_assert(has_pre_decrement_operator_v<It>, "Iterator must have pre-decrement operator");
     };
 }
