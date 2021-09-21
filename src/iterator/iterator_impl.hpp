ce10def7321cf08e3f4ee7303585064e608e89ef
Idolin <dk.1997-fast@yandex.ru>
2021-09-22 01:32:26 +0300

    Implemented _preDecrement and _postDecrement structures
    Few more asserts
    
    Added get_difference_type type trait
    
    Added has_get_index_method and has_set_index_method type traits

--- src/iterator/iterator_impl.hpp
+++ src/iterator/iterator_impl.hpp
@@ -2,15 +2,15 @@
 
 #include "../template/type_tags.hpp"
 
+#include <cstddef>
 #include <type_traits>
 #include <utility>
-
-#include <iostream>
 
 namespace iterator_impl_def
 {
     namespace
     {
+
         template<typename It>
         struct get_def_value
         {
@@ -42,6 +42,58 @@ namespace iterator_impl_def
 
             typedef decltype(std::declval<It>().operator->()) type;
         };
+
+        template<typename It, typename Enable = void>
+        struct get_def_difference
+        {
+            typedef std::ptrdiff_t type;
+        };
+
+        template<typename It>
+        struct get_def_difference<It, typename std::enable_if_t<is_subtractable_v<It>>>
+        {
+            static_assert(is_subtractable_v<const It>, "Random access iterator It: this subtract operation must be valid: const It - const It");
+
+            typedef decltype(std::declval<It>() - std::declval<It>()) type;
+        };
+
+        template<typename It, typename Enable = void>
+        struct has_get_index_method
+        {
+            enum
+            {
+                value = false
+            };
+        };
+
+        template<typename It>
+        struct has_get_index_method<It,
+                typename std::enable_if_t<std::is_integral<decltype(std::declval<It>().getIndex())>::value>>
+        {
+            enum
+            {
+                value = true
+            };
+        };
+
+        template<typename It, typename Enable = void>
+        struct has_set_index_method
+        {
+            enum
+            {
+                value = false
+            };
+        };
+
+        template<typename It>
+        struct has_set_index_method<It,
+                decltype(std::declval<It>().setIndex(std::declval<get_def_difference<It>::type>()), void())>
+        {
+            enum
+            {
+                value = true
+            };
+        };
     }
 
     template<typename It>
@@ -53,12 +105,16 @@ namespace iterator_impl_def
     template<typename It>
     using get_def_pointer_t = typename get_def_pointer<It>::type;
 
+    template<typename It>
+    using get_def_difference_t = typename get_def_difference<It>::type;
 
     template<typename Impl, typename It, typename Enable = void>
     struct _notEqualDefault
     {
-        static_assert(has_equality_operator_v<Impl>, "Iterator must have bool operator==");
-        static_assert(has_equality_operator_v<const Impl>, "Iterator's operator== must be const");
+        static_assert(has_equality_operator_v<Impl>, "Input iterator must have bool operator==");
+        static_assert(has_equality_operator_v<const Impl>, "Input iterator It: operator== must allow to compare const It as well");
+        static_assert(std::is_convertible<decltype(std::declval<Impl>() == std::declval<Impl>()), bool>::value,
+            "Input iterator operator== must return type implicitly convertible to bool");
 
         bool operator!=(const It& otr) const
         {
@@ -69,8 +125,10 @@ namespace iterator_impl_def
     template<typename Impl, typename It>
     struct _notEqualDefault<Impl, It, typename std::enable_if_t<has_inequality_operator_v<Impl>>>
     {
-        static_assert(has_equality_operator_v<Impl>, "Iterator must have bool operator==");
-        static_assert(has_equality_operator_v<const Impl>, "Iterator's operator== must be const");
+        static_assert(has_equality_operator_v<Impl>, "Input iterator must have bool operator==");
+        static_assert(has_equality_operator_v<const Impl>, "Input iterator It: operator== must allow to compare const It as well");
+        static_assert(std::is_convertible<decltype(std::declval<Impl>() == std::declval<Impl>()), bool>::value,
+            "Input iterator operator== must return type implicitly convertible to bool");
     };
 
 
@@ -145,7 +203,7 @@ namespace iterator_impl_def
     template<typename Impl, typename It, typename Enable = void>
     struct _preIncrementDefault
     {
-        static_assert(!has_pre_increment_operator_v<Impl> || !has_increment_method_v<Impl>,
+        static_assert(has_increment_method_v<Impl>,
             "Iterator must have either pre-increment operator or increment method");
 
         void _preIncrement()
@@ -157,7 +215,7 @@ namespace iterator_impl_def
     template<typename Impl, typename It>
     struct _preIncrementDefault<Impl, It, typename std::enable_if_t<has_pre_increment_operator_v<Impl>>>
     {
-        static_assert(!has_pre_increment_operator_v<Impl> || !has_increment_method_v<Impl>,
+        static_assert(!has_increment_method_v<Impl>,
             "Iterator must have either pre-increment operator or increment method, not both");
 
         void _preIncrement()
@@ -167,22 +225,77 @@ namespace iterator_impl_def
     };
 
 
-    template<typename It, typename R, typename Enable = void>
+    template<typename Impl, typename It, typename Enable = void>
     struct _postDecrementDefault
     {
-        static_assert(has_pre_decrement_operator_v<It>, "Iterator must have pre-decrement operator");
+        static_assert(!has_pre_decrement_operator_v<Impl> || !has_decrement_method_v<Impl>,
+            "Bidirectional iterator must have either pre-decrement operator or decrement method");
 
-        R operator--(int)
+        It _postDecrement()
         {
-            R copy = *static_cast<R*>(this);
-            static_cast<It*>(this)->decrement();
+            It copy = *static_cast<It*>(this);
+            (*static_cast<It*>(this)).Impl::operator--();
             return copy;
         }
     };
 
-    template<typename It, typename R>
-    struct _postDecrementDefault<It, R, typename std::enable_if_t<has_post_decrement_operator_v<It>>>
+    template<typename Impl, typename It>
+    struct _postDecrementDefault<Impl, It, typename std::enable_if_t<has_decrement_method_v<Impl> && !has_post_decrement_operator_v<Impl>>>
     {
-        static_assert(has_pre_decrement_operator_v<It>, "Iterator must have pre-decrement operator");
+        It _postDecrement()
+        {
+            It copy = *static_cast<It*>(this);
+            (*static_cast<It*>(this)).Impl::decrement();
+            return copy;
+        }
+    };
+
+    template<typename Impl, typename It>
+    struct _postDecrementDefault<Impl, It, typename std::enable_if_t<has_post_decrement_operator_v<Impl>>>
+    {
+    private:
+        // Impl::operator--(int) return type
+        typedef decltype(std::declval<Impl>()--) post_decrement_t;
+
+        // type iterator helper(It) will return for operator--(int): either It or Impl::operator--(int) return type
+        typedef std::conditional_t<
+            is_same_omit_cv_v<post_decrement_t, Impl>,
+                copy_cv_refernce_t<Impl, It>, post_decrement_t> post_decrement_ret;
+
+    public:
+        static_assert(!has_post_decrement_operator_v<Impl> || !has_decrement_method_v<Impl>,
+            "Bidirectional iterator must have either post-decrement operator or decrement method, not both");
+        static_assert(is_dereferencable_v<post_decrement_t>,
+            "Bidirectional iterator post-decrement operator must return dereferencable object");
+
+        post_decrement_ret _postDecrement()
+        {
+            return (*static_cast<It*>(this)).Impl::operator--(0);
+        }
+    };
+
+
+    template<typename Impl, typename It, typename Enable = void>
+    struct _preDecrementDefault
+    {
+        static_assert(has_decrement_method_v<Impl>,
+            "Bidirectional iterator must have either pre-decrement operator or decrement method");
+
+        void _preDecrement()
+        {
+            (*static_cast<It*>(this)).Impl::decrement();
+        }
+    };
+
+    template<typename Impl, typename It>
+    struct _preDecrementDefault<Impl, It, typename std::enable_if_t<has_pre_decrement_operator_v<Impl>>>
+    {
+        static_assert(!has_decrement_method_v<Impl>,
+            "Bidirectional iterator must have either pre-decrement operator or decrement method, not both");
+
+        void _preDecrement()
+        {
+           (*static_cast<It*>(this)).Impl::operator--();
+        }
     };
 }
