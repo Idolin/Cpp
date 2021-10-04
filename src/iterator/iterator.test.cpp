ec14de5d178fccf552055dd15ef0b0b3653a5530
Idolin <dk.1997-fast@yandex.ru>
2021-10-04 15:43:06 +0300

    Add forward iterator adapter tests

--- src/iterator/iterator.test.cpp
+++ src/iterator/iterator.test.cpp
@@ -7,6 +7,216 @@
 
 TEST_PACK(iterator)
 {
+    namespace
+    {
+
+        struct SomeInputIterator
+        {
+            struct IntWrapper
+            {
+                int x;
+
+                int operator*()
+                {
+                    return x;
+                }
+            };
+
+            typedef std::input_iterator_tag iterator_category;
+            typedef const int value_type;
+            typedef const int& reference;
+            typedef const int* pointer;
+            typedef std::ptrdiff_t difference_type;
+
+            int r = 0;
+
+            SomeInputIterator(int x): r(x)
+            {}
+
+            const int& operator*() const
+            {
+                return r;
+            }
+
+            const int* operator->() const
+            {
+                return &r;
+            }
+
+            SomeInputIterator& operator++()
+            {
+                r++;
+                return *this;
+            }
+
+            IntWrapper operator++(int)
+            {
+                IntWrapper copy{r};
+                r++;
+                return copy;
+            }
+
+            bool operator==(const SomeInputIterator& otr) const
+            {
+                return r == otr.r;
+            }
+
+            bool operator!=(const SomeInputIterator& otr) const
+            {
+                return r != otr.r;
+            }
+        };
+
+        struct InputIteratorImpl
+        {
+            int r = 0;
+
+            const int& operator*() const
+            {
+                return r;
+            }
+
+            void increment()
+            {
+                r++;
+            }
+
+            bool operator==(const InputIteratorImpl& otr) const
+            {
+                return r == otr.r;
+            }
+        };
+
+        struct GetSetIndexIteratorImpl
+        {
+            int *a = new int[]{3, 5, 7, 9, 11, 13, 15};
+            int index = 0;
+
+            ~GetSetIndexIteratorImpl()
+            {
+                delete[] a;
+            }
+
+            int& operator*() const
+            {
+                return a[index];
+            }
+
+            int getIndex() const
+            {
+                return index;
+            }
+
+            void setIndex(int new_index)
+            {
+                index = new_index;
+            }
+        };
+
+        struct GetIndexIncrementDecrementMethodIteratorImpl
+        {
+            int *collatz = new int[]{7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1};
+            int index = 0;
+
+            ~GetIndexIncrementDecrementMethodIteratorImpl()
+            {
+                delete[] collatz;
+            }
+
+            int& operator*() const
+            {
+                return collatz[index];
+            }
+
+            int getIndex() const
+            {
+                return index;
+            }
+
+            void increment(int value = 1)
+            {
+                index += value;
+            }
+
+            void decrement(int value = 1)
+            {
+                index -= value;
+            }
+        };
+
+        struct MixedIteratorImpl
+        {
+            struct BoolWrapper
+            {
+                bool v;
+
+                BoolWrapper(bool f): v(f)
+                {}
+
+                explicit operator bool() const
+                {
+                    return v;
+                }
+            };
+
+            int *not_random = new int[]{0, 1, 2, 0, 0, 4, 2, 1, 2, 4, 2, 5, 8, 0, 1, 4, 5, 5, 4, 4, 1, 4, 4, 4, 0, 8, 0, 8, 2, 5, 0, 2, 2, 1, 4, 2, 2, 5, 1, 5, 8, 4, 5, 5};
+            int index = 0;
+
+            ~MixedIteratorImpl()
+            {
+                delete[] not_random;
+            }
+
+            int& operator*() const
+            {
+                return not_random[index];
+            }
+
+            MixedIteratorImpl& operator++()
+            {
+                index++;
+                return *this;
+            }
+
+            void increment(int value)
+            {
+                index += value;
+            }
+
+            void decrement()
+            {
+                index--;
+            }
+
+            MixedIteratorImpl& operator-=(int value)
+            {
+                index -= value;
+                return *this;
+            }
+
+            bool operator==(const MixedIteratorImpl& otr) const
+            {
+                return index == otr.index;
+            }
+
+            bool operator!=(const MixedIteratorImpl& otr) const
+            {
+                return index % 3 != otr.index % 3;
+            }
+
+            bool operator<(const MixedIteratorImpl& otr) const
+            {
+                return index < otr.index;
+            }
+
+            BoolWrapper operator<=(const MixedIteratorImpl& otr) const
+            {
+                return (index < otr.index) && (index % 3 != otr.index % 3);
+            }
+        };
+
+    }
+
     TEST(iterator_checks_empty)
     {
         struct Empty
@@ -185,83 +395,6 @@ TEST_PACK(iterator)
             }
         };
 
-        struct SomeInputIterator
-        {
-            struct IntWrapper
-            {
-                int x;
-
-                int operator*()
-                {
-                    return x;
-                }
-            };
-
-            typedef std::input_iterator_tag iterator_category;
-            typedef const int value_type;
-            typedef const int& reference;
-            typedef const int* pointer;
-            typedef std::ptrdiff_t difference_type;
-
-            int r = 0;
-
-            SomeInputIterator(int x): r(x)
-            {}
-
-            const int& operator*() const
-            {
-                return r;
-            }
-
-            const int* operator->() const
-            {
-                return &r;
-            }
-
-            SomeInputIterator& operator++()
-            {
-                r++;
-                return *this;
-            }
-
-            IntWrapper operator++(int)
-            {
-                IntWrapper copy{r};
-                r++;
-                return copy;
-            }
-
-            bool operator==(const SomeInputIterator& otr) const
-            {
-                return r == otr.r;
-            }
-
-            bool operator!=(const SomeInputIterator& otr) const
-            {
-                return r != otr.r;
-            }
-        };
-
-        struct InputIteratorImpl
-        {
-            int r = 0;
-
-            const int& operator*() const
-            {
-                return r;
-            }
-
-            void increment()
-            {
-                r++;
-            }
-
-            bool operator==(const InputIteratorImpl& otr) const
-            {
-                return r == otr.r;
-            }
-        };
-
         using InputIterator = input_iterator<InputIteratorImpl>;
 
 
@@ -301,5 +434,23 @@ TEST_PACK(iterator)
         EXPECT_EQ(*it, 3);
         it = it_copy2;
         EXPECT_EQ(*it, 2);
+    }
+
+    TEST(forward_iterator)
+    {
+        using ForwardIterator = forward_iterator<GetSetIndexIteratorImpl>;
+        using ForwardIterator2 = forward_iterator<GetIndexIncrementDecrementMethodIteratorImpl>;
+        using ForwardIterator3 = forward_iterator<MixedIteratorImpl>;
+
+        EXPECT_TRUE(is_valid_stl_iterator_v<ForwardIterator>);
+        EXPECT_TRUE(is_valid_stl_forward_iterator_v<ForwardIterator>);
+        EXPECT_TRUE(is_valid_stl_iterator_v<ForwardIterator2>);
+        EXPECT_TRUE(is_valid_stl_forward_iterator_v<ForwardIterator2>);
+        EXPECT_TRUE(is_valid_stl_iterator_v<ForwardIterator3>);
+        EXPECT_TRUE(is_valid_stl_forward_iterator_v<ForwardIterator3>);
+
+        ForwardIterator it_gs = ForwardIterator();
+
+        EXPECT_EQ(*it_gs, 3);
     }
 }
