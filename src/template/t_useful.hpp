107ff86e5cad2abc79b7ada0ff75408ae3b0a64d
Idolin <dk.1997-fast@yandex.ru>
2022-04-04 18:03:36 +0300

    Added DefaultTypeWrapper to check whether the type is explicitly defined
    Added FirstTypeIf struct for a more concise and clear type inference

--- src/template/t_useful.hpp
+++ src/template/t_useful.hpp
@@ -31,6 +31,106 @@ struct rank_p<0>
     };
 };
 
+
+namespace
+{
+
+    struct _Wrapper_Def_Tag
+    {};
+
+
+    template<bool get, typename T>
+    struct _GetTypeTypedefIf;
+
+    template<typename T>
+    struct _GetTypeTypedefIf<true, T>
+    {
+        typedef typename T::type type;
+    };
+
+    template<typename T>
+    struct _GetTypeTypedefIf<false, T>
+    {
+        typedef T type;
+    };
+
+
+    template<typename TTrait, typename Enable = void>
+    struct _GetMemberTypedefedType
+    {
+        static constexpr bool value = false;
+    };
+
+    template<typename TTrait>
+    struct _GetMemberTypedefedType<TTrait, std::void_t<typename TTrait::type>>
+    {
+        static constexpr bool value = true;
+
+        typedef typename TTrait::type type;
+    };
+
+}
+
+/*
+ * Used to differenciate between explicitly provided types and default ones in template arguments
+ * Usage: template<typename T = TypeWrapperDefault<DefaultType>>
+ */
+template<typename T>
+struct DefaultTypeWrapper: _Wrapper_Def_Tag
+{
+    typedef T type;
+};
+
+template<typename W>
+constexpr bool is_wrapped_v = std::is_base_of<_Wrapper_Def_Tag, W>::value;
+
+template<typename W>
+using get_wrapped_t = typename _GetTypeTypedefIf<is_wrapped_v<W>, W>::type;
+
+
+/*
+ * FirstTypeIf:
+ *  if enable is true, then
+ *      FirstType::type = TypeTraitN::type,
+ *          where TypeTraitN is first struct in TypeTraitsToTry list which has type typedef
+ *      If no struct in TypeTraitsToTry list has type typedef, then this struct is empty
+ *  if enable is false, then this struct is empty
+ */
+template<bool enable, typename... TypeTraitsToTry>
+struct FirstTypeIf
+{};
+
+template<typename TFirst, typename... TOthers>
+struct FirstTypeIf<true, TFirst, TOthers...>:
+        std::conditional_t<_GetMemberTypedefedType<TFirst>::value,
+            _GetMemberTypedefedType<TFirst>,
+            FirstTypeIf<!_GetMemberTypedefedType<TFirst>::value, TOthers...>>
+{};
+
+
+// FirstType<TypeTraitsToTry...> equivalent to FirstTypeIf<true, TypeTraitsToTry...>
+template<typename... TypeTraitsToTry>
+using FirstType = FirstTypeIf<true, TypeTraitsToTry...>;
+
+
+// Helper struct for FirstTypeIf trait
+template<bool enable, typename T>
+struct TypeIf
+{};
+
+template<typename T>
+struct TypeIf<true, T>
+{
+    typedef T type;
+};
+
+// Helper struct for FirstTypeIf trait
+template<typename T>
+struct ReturnType
+{
+    typedef T type;
+};
+
 namespace t_adapter
 {
     template<int n, typename... Types>
@@ -47,6 +147,7 @@ namespace t_adapter
     {
         typedef First type;
     };
+
     
     struct t_tuple_tag
     {};
@@ -64,6 +165,7 @@ namespace t_adapter
             typedef typename get_type<n, Types...>::type type;
         };
     };
+
     
     template<typename T, typename Enable = void>
     struct make_t_tuple;
