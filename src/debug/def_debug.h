9f0627517537d5c85fed0a00b7281779bf650f51
Idolin <dk.1997-fast@yandex.ru>
2022-06-08 14:33:25 +0300

    Some changes in macros to conform to C++14 standard
    Renamed ASSERT_CODE_OK to ASSERT_CODE_ZERO

--- src/debug/def_debug.h
+++ src/debug/def_debug.h
@@ -40,71 +40,75 @@ private:
 
 #ifdef DEBUG
 
-#ifndef DEBUG_LVL
-#define DEBUG_LVL 5
-#endif
-#define DEBUGLVLMSG_N(level, ...) \
-    { \
-        if(level <= DEBUG_LVL) \
+    #ifndef DEBUG_LVL
+        #define DEBUG_LVL 5
+    #endif
+
+    #define DEBUGLVLMSG_N(level, ...) \
         { \
-            if(level <= 1) \
-                fprintf(stderr, ## __VA_ARGS__); \
-            else \
-                fprintf(DEBUG_OUTPUT_STREAM, ## __VA_ARGS__); \
-        } \
-    }
-#define DEBUGLVLMSG(level, ...) \
-    { \
-        DEBUGLVLMSG_N(level, ## __VA_ARGS__); \
-        if(level <= DEBUG_LVL) \
+            if(level <= DEBUG_LVL) \
+            { \
+                if(level <= 1) \
+                    fprintf(stderr, __VA_ARGS__); \
+                else \
+                    fprintf(DEBUG_OUTPUT_STREAM, __VA_ARGS__); \
+            } \
+        }
+    #define DEBUGLVLMSG(level, ...) \
         { \
-            if(level <= 1) \
-                fputc('\n', stderr); \
-            else \
-                fputc('\n', DEBUG_OUTPUT_STREAM); \
-        } \
-    }
-#define DEBUGMSG(...) DEBUGLVLMSG(5, ## __VA_ARGS__)
-#define DEBUGLVLIFMSG(level, cond, ...) \
-    { \
-        if(cond) \
-            DEBUGLVLMSG(level, ## __VA_ARGS__); \
-    }
-#define DEBUGIFMSG(cond, ...) DEBUGLVLIFMSG(4, cond, ## __VA_ARGS__)
-// ASSERT_NO_THROW(condition[, printf args...])
-#define ASSERT_NO_THROW(...) \
-    { \
-        if(!(GET_FIRST(__VA_ARGS__))) \
+            DEBUGLVLMSG_N(level, __VA_ARGS__); \
+            if(level <= DEBUG_LVL) \
+            { \
+                if(level <= 1) \
+                    fputc('\n', stderr); \
+                else \
+                    fputc('\n', DEBUG_OUTPUT_STREAM); \
+            } \
+        }
+    #define DEBUGMSG(...) DEBUGLVLMSG(5, __VA_ARGS__)
+    #define DEBUGLVLIFMSG(level, condition, ...) \
         { \
-            set_term_color(term_color::RED, stderr); \
-            DEBUGLVLMSG(1, "%s %d in file %s!", "Assertion failed at line", __LINE__, __FILE__); \
-            set_term_color(term_color::DEFAULT, stderr); \
-            FCALL_IF_ARGS_GT_N_L(1, DEBUGLVLMSG, 1 CSKIP_1L(__VA_ARGS__)); \
-            fflush(DEBUG_OUTPUT_STREAM); \
-        } \
-    }
-#define ASSERT_ERR(cond, err_code, ...) \
-    { \
-        if(!(cond)) \
+            if(condition) \
+                DEBUGLVLMSG(level, __VA_ARGS__); \
+        }
+    #define DEBUGIFMSG(condition, ...) DEBUGLVLIFMSG(4, condition, __VA_ARGS__)
+    // ASSERT_NO_THROW(condition[, printf args...])
+    #define ASSERT_NO_THROW(...) \
         { \
-            ASSERT_NO_THROW(false, ## __VA_ARGS__); \
-            throw assert_failed("Assertion failed!", __FILE__, __LINE__, (err_code)); \
-        } \
-    }
-#define ASSERT(cond, ...) ASSERT_ERR((cond), -1, ## __VA_ARGS__)
-#define ASSERT_CODE_NOT(func, fail_code, ...) ASSERT((func) != (fail_code), ## __VA_ARGS__)
-#define ASSERT_CODE_OK(func, ...) ASSERT((func) == 0, ## __VA_ARGS__)
+            if(!(GET_FIRST(__VA_ARGS__))) \
+            { \
+                set_term_color(term_color::RED, stderr); \
+                DEBUGLVLMSG(1, "%s %d in file %s!", "Assertion failed at line", __LINE__, __FILE__); \
+                set_term_color(term_color::DEFAULT, stderr); \
+                FCALL_IF_ARGS_GT_N_L(1, DEBUGLVLMSG, 1 CSKIP_1L(__VA_ARGS__)); \
+                fflush(DEBUG_OUTPUT_STREAM); \
+            } \
+        }
+    // ASSERT_ERR(condition, error_code[, printf args...])
+    #define ASSERT_ERR(condition, ...) \
+        { \
+            if(!(condition)) \
+            { \
+                ASSERT_NO_THROW(false CSKIP_1L(__VA_ARGS__)); \
+                throw assert_failed("Assertion failed!", __FILE__, __LINE__, (GET_FIRST(__VA_ARGS__))); \
+            } \
+        }
+    // ASSERT(condition[, printf args...])
+    #define ASSERT(...) ASSERT_ERR((GET_FIRST(__VA_ARGS__)), -1 CSKIP_1L(__VA_ARGS__))
+    // ASSERT_CODE_ZERO(expression[, printf args...]) -> invokes ASSERT if expression returns non-zero code
+    #define ASSERT_CODE_ZERO(...) ASSERT((GET_FIRST(__VA_ARGS__)) == 0 CSKIP_1L(__VA_ARGS__))
 
 #else
-#define DEBUGLVLMSG_N(level, ...) {}
-#define DEBUGLVLMSG(level, ...) {}
-#define DEBUGMSG(...) {}
-#define DEBUGLVLIFMSG(level, cond, ...) {}
-#define DEBUGIFMSG(cond, ...) {}
-#define ASSERT_NO_THROW(cond, ...) {}
-#define ASSERT_ERR(cond, err_code, ...) {}
-#define ASSERT(cond, ...) {}
-#define ASSERT_CODE_NOT(func, fail_code, ...) func;
-#define ASSERT_CODE_OK(func, ...) func;
+
+    #define DEBUGLVLMSG_N(level, ...) {}
+    #define DEBUGLVLMSG(level, ...) {}
+    #define DEBUGMSG(...) {}
+    #define DEBUGLVLIFMSG(level, cond, ...) {}
+    #define DEBUGIFMSG(cond, ...) {}
+    #define ASSERT_NO_THROW(...) {}
+    #define ASSERT_ERR(condition, ...) {}
+    #define ASSERT(...) {}
+    #define ASSERT_CODE_ZERO(...) func;
+
 #endif
 
