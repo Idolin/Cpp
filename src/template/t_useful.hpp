9c03943b77b2a70a78d778f9c6eb19b555e88546
Idolin <dk.1997-fast@yandex.ru>
2022-06-27 23:22:15 +0300

    Added InstantiateIf helper structs for type deduction
    Removed now unused t_adapter namespace

--- src/template/t_useful.hpp
+++ src/template/t_useful.hpp
@@ -141,96 +141,19 @@ struct TypeIf<true, T>
     typedef T type;
 };
 
+template<bool enable, template<typename... Ts> class T, typename... Args>
+struct InstantiateIf
+{};
+
+template<template<typename... Ts> class T, typename... Args>
+struct InstantiateIf<true, T, Args...>
+{
+    typedef T<Args...> type;
+};
+
 // Helper struct for FirstTypeIf trait
 template<typename T>
 struct ReturnType
 {
     typedef T type;
 };
-
-namespace t_adapter
-{
-    template<int n, typename... Types>
-    struct get_type;
-
-    template<int n, typename First, typename... Types>
-    struct get_type<n, First, Types...>
-    {
-        typedef typename get_type<n - 1, Types...>::type type;
-    };
-
-    template<typename First, typename... Types>
-    struct get_type<0, First, Types...>
-    {
-        typedef First type;
-    };
-
-    
-    struct t_tuple_tag
-    {};
-
-    template<typename... Types>
-    struct t_tuple: t_tuple_tag
-    {
-        // typedef typename get_type<0, Types...>::type T1;
-        // typedef typename get_type<1, Types...>::type T2;
-        // typedef typename get_type<2, Types...>::type T3;
-
-        template<int n>
-        struct type_n
-        {
-            typedef typename get_type<n, Types...>::type type;
-        };
-    };
-
-    
-    template<typename T, typename Enable = void>
-    struct make_t_tuple;
-    
-    template<typename T>
-    struct make_t_tuple<T, std::enable_if_t<std::is_base_of<t_tuple_tag, T>::value>>
-    {
-        typedef T type;
-    };
-    
-    template<typename T>
-    struct make_t_tuple<T, std::enable_if_t<!std::is_base_of<t_tuple_tag, T>::value>>
-    {
-        typedef t_tuple<T> type;
-    };
-
-    template<typename... Types>
-    struct once
-    {
-        typedef t_tuple<Types...> TypeSeq;
-
-        typedef void NextAdapter;
-    };
-
-    template<typename... Types>
-    struct twice
-    {
-        typedef t_tuple<Types...> TypeSeq;
-
-        typedef once<Types...> NextAdapter;
-    };
-    
-    template<typename... Types>
-    struct seq;
-    
-    template<typename First, typename... Types>
-    struct seq<First, Types...>
-    {
-        typedef typename make_t_tuple<First>::type TypeSeq;
-        
-        typedef seq<Types...> NextAdapter;
-    };
-    
-    template<typename First>
-    struct seq<First>
-    {
-        typedef typename make_t_tuple<First>::type TypeSeq;
-        
-        typedef void NextAdapter;
-    };
-}
